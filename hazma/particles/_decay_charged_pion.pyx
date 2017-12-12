1044bfeaf1c5b349febbc5935975d02ce4aebb2e
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2017-12-11 22:41:18 -0800

    Cleaning up charged pion files

--- hazma/particles/_decay_charged_pion.pyx
+++ hazma/particles/_decay_charged_pion.pyx
@@ -2,7 +2,6 @@ cimport _decay_muon
 import numpy as np
 cimport numpy as np
 from scipy.integrate import quad
-from scipy.interpolate import interp1d
 from libc.math cimport exp, log, M_PI, log10, sqrt, abs
 import cython
 from functools import partial
