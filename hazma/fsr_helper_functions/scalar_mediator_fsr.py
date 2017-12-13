39ae705d555b8e89365c4ef3bdec68592d7f001a
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2017-12-12 22:52:24 -0800

    Updating docstring for pseudo-scalar mediator fsr.

--- hazma/fsr_helper_functions/scalar_mediator_fsr.py
+++ hazma/fsr_helper_functions/scalar_mediator_fsr.py
@@ -7,23 +7,23 @@ def fermion(eng_gam, cme, mass_f):
     """
     Return the fsr spectra for fermions from decay of scalar mediator.
 
-    Computes the final state radiaton spectrum value dNdE given a gamma
-    ray energy of `eng_gam`, center of mass energy `cme` and final state
-    fermion mass `mass_f`.
+    Computes the final state radiaton spectrum value dNdE from a scalar
+    mediator given a gamma ray energy of `eng_gam`, center of mass energy `cme`
+    and final state fermion mass `mass_f`.
 
     Paramaters
     ----------
     eng_gam : float
         Gamma ray energy.
     cme: float
-        Center of mass energy of mass of off-shell mediator.
+        Center of mass energy of mass of off-shell scalar mediator.
     mass_f : float
         Mass of the final state fermion.
 
     Returns
     -------
     spec_val : float
-        Spectrum value dNdE.
+        Spectrum value dNdE from scalar mediator.
     """
     val = 0.0
 
