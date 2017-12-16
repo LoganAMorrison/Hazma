6be5c4515cb2cbb6f58ff9545e24f5a4fedb1cb7
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2017-12-15 16:24:19 -0800

    Adding pure c functions for function calling.

--- hazma/decay_helper_functions/decay_neutral_pion.pyx
+++ hazma/decay_helper_functions/decay_neutral_pion.pyx
@@ -9,6 +9,43 @@ Module for computing the decay spectrum from neutral pion.
 """
 
 @cython.cdivision(True)
+cdef double CSpectrumPoint(eng_gam, float eng_pi):
+    """
+    Returns decay spectrum for pi0 -> g g.
+    """
+    cdef float beta = sqrt(1.0 - (MASS_PI0 / eng_pi)**2)
+
+    cdef float ret_val = 0.0
+
+    if eng_pi * (1 - beta) / 2.0 <= eng_gam and \
+            eng_gam <= eng_pi * (1 + beta) / 2.0:
+        ret_val = BR_PI0_TO_GG * 2.0 / (eng_pi * beta)
+
+    return ret_val
+
+@cython.cdivision(True)
+cdef double CSpectrum(np.ndarray eng_gam, float eng_pi):
+    """
+    Returns decay spectrum for pi0 -> g g.
+    """
+    cdef float beta = sqrt(1.0 - (MASS_PI0 / eng_pi)**2)
+    cdef int numpts = len(eng_gam)
+    cdef np.ndarray spec = np.zeros(numpts, float)
+    cdef int i
+    cdef float ret_val
+
+    for i in range(numpts):
+
+        ret_val = 0.0
+
+        if eng_pi * (1 - beta) / 2.0 <= eng_gam[i] and \
+                eng_gam[i] <= eng_pi * (1 + beta) / 2.0:
+            ret_val = BR_PI0_TO_GG * 2.0 / (eng_pi * beta)
+        spec[i] = ret_val
+
+    return spec
+
+@cython.cdivision(True)
 def SpectrumPoint(eng_gam, float eng_pi):
     """
     Returns decay spectrum for pi0 -> g g.
