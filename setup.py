a6389d34d6d71ddd590512a019ebfe905c2631be
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2017-12-17 00:30:49 -0800

    Update setup.py information.

--- setup.py
+++ setup.py
@@ -12,12 +12,21 @@ extensions = ["hazma/decay_helper_functions/*.pyx",
 
 setup(name='hazma',
       version='1.0',
-      description='Gamma Ray Spectrum Generator',
       author='Logan Morrison and Adam Coogan',
       author_email='loanmorr@ucsc.edu',
-      url='',
+      maintainer='Logan Morrison',
+      maintainer_email='loanmorr@ucsc.edu',
+      url='http://hazma.readthedocs.io/en/latest/',
+      description='Package for computing FSR and decay spectra for light \
+      particles',
+      long_description="""Package for computing the FSR and decay spectra for
+      light mesons (pions and kaons) and light fermions (electron and muon).
+      """,
       packages=packs,
       ext_modules=cythonize(extensions),
       include_dirs=[np.get_include(), 'hazma/decay_helper_functions'],
-      include_package_data=True
+      include_package_data=True,
+      license='MIT License',
+      platforms='MacOS and Linux',
+      download_url='https://github.com/LoganAMorrison/Hazma'
       )
