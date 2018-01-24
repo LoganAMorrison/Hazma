adfb06a1ba30d9820c748b747fc4d7e766017680
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2018-01-24 10:47:27 -0800

    Updating testing modules

--- hazma/test/test_fsr.py
+++ hazma/test/test_fsr.py
@@ -1,7 +1,22 @@
-from hazma import muon
+from hazma.fsr_helper_functions import scalar_mediator_fsr
+from hazma.fsr_helper_functions import pseudo_scalar_mediator_fsr
+from hazma.fsr_helper_functions import vector_mediator_fsr
+
+from ..parameters import muon_mass
+
 import numpy as np
 
 
-def test_muon_fsr():
+def test_scalar_mediator_fsr():
     eng_gams = np.logspace(-3., 3., num=100)
-    muon.fsr(eng_gams, 1000.)
+    scalar_mediator_fsr(eng_gams, 1000., muon_mass)
+
+
+def test_pseudo_scalar_mediator_fsr():
+    eng_gams = np.logspace(-3., 3., num=100)
+    pseudo_scalar_mediator_fsr(eng_gams, 1000., muon_mass)
+
+
+def test_vector_mediator_fsr():
+    eng_gams = np.logspace(-3., 3., num=100)
+    vector_mediator_fsr(eng_gams, 1000., muon_mass)
