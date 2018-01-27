ccb532b2576fc1ac430cc5420dcd6ce5a7b72adc
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2018-01-26 20:01:34 -0800

    Updated documentation for new functions.

--- hazma/fsr_helper_functions/scalar_mediator_fsr.py
+++ hazma/fsr_helper_functions/scalar_mediator_fsr.py
@@ -5,7 +5,6 @@
 
 """
 import numpy as np
-from scipy.integrate import quad
 import os
 
 from ..parameters import vh, b0, alpha_em, fpi
@@ -72,21 +71,71 @@ def fermion(eng_gam, cme, mass_f):
 
 """ Charged Pion FSR """
 
+# Set path for the unitarized squared matrix element for pi pi -> pi pi data
+# file
 DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                          "unitarization",
                          "final_state_int_unitarizated.dat")
 
+# Load the data for the unitarized squared matrix element for pi pi -> pi pi.
 unitarizated_data = np.genfromtxt(DATA_PATH, delimiter=',')
 
 
 def unit_matrix_elem_sqrd(cme):
+    """
+    Returns the unitarized squared matrix element for :math:`\pi\pi\to\pi\pi`
+    divided by the leading order, ununitarized squared matrix element for
+    :math:`\pi\pi\to\pi\pi`.
+
+    Parameters
+    ----------
+    cme : double
+        Invariant mass of the two charged pions.
+
+    Results
+    -------
+    unit_matrix_elem_sqrd : double
+        The unitarized matrix element for :math:`\pi\pi\to\pi\pi`, |t_u|^2,
+        divided by the un-unitarized squared matrix element for
+        :math:`\pi\pi\to\pi\pi`, |t|^2; |t_u|^2 / |t|^2.
+    """
     t_mod_sqrd = np.interp(cme, unitarizated_data[0], unitarizated_data[1])
     additional_factor = (2 * cme**2 - mpi**2) / (32. * fpi**2 * np.pi)
     return t_mod_sqrd / additional_factor**2
 
 
 def __xx_to_s_pipig_mat_elem(s, t, Q, mx, ms, cxxs, cffs, cggs, vs):
+    """
+    Returns the squared matrix element for two fermions annihilating into two
+    charged pions and a photon.
 
+    Parameters
+    ----------
+    s : double
+        Mandelstam variable associated with the photon, defined as (P-pg)^2,
+        where P = ppip + ppim + pg.
+    t : double
+        Mandelstam variable associated with the charged pion, defined as
+        (P-ppip)^2, where P = ppip + ppim + pg.
+    Q : double
+        Center of mass energy, or sqrt((ppip + ppim + pg)^2).
+    mx : double
+        Mass of the initial state fermion.
+    ms : double
+        Mass of the scalar mediator.
+    cxxs : double
+        Coupling of the initial state fermion to the scalar mediator.
+    cffs : double
+    cggs : double
+        Coupling of the scalar to gluons.
+    vs : double
+        Vacuum expectation value of the scalar mediator.
+
+    Returns
+    -------
+    Returns matrix element squared for
+    :math:`\chi\bar{\chi}\to\pi^{+}\pi^{-}\gamma`.
+    """
     u = u_to_st(0.0, mpi, mpi, Q, s, t)
 
     mat_elem_sqrd = (-16 * cxxs**2 *
@@ -107,28 +156,78 @@ def __xx_to_s_pipig_mat_elem(s, t, Q, mx, ms, cxxs, cffs, cggs, vs):
     return mat_elem_sqrd * unit_matrix_elem_sqrd(np.sqrt(s))
 
 
-def __xx_to_s_pipi_xsec(s, mx, ms, cxxs, cffs, cggs, vs):
-    mat_elem_sqrd = (-2 * cxxs**2 * (4 * mx**2 - s) *
-                     (2 * cggs * (2 * mpi**2 - s) *
+def __xx_to_s_pipi_xsec(cme, mx, ms, cxxs, cffs, cggs, vs):
+    """
+    Returns the squared matrix element for two fermions annihilating into two
+    charged pions.
+
+    Parameters
+    ----------
+    cme : double
+        Center of mass energy.
+    mx : double
+        Mass of the initial state fermion.
+    ms : double
+        Mass of the scalar mediator.
+    cxxs : double
+        Coupling of the initial state fermion to the scalar mediator.
+    cffs : double
+    cggs : double
+        Coupling of the scalar to gluons.
+    vs : double
+        Vacuum expectation value of the scalar mediator.
+
+    Returns
+    -------
+    Returns matrix element squared for :math:`\chi\bar{\chi}\to\pi^{+}\pi^{-}`.
+    """
+    mat_elem_sqrd = (-2 * cxxs**2 * (4 * mx**2 - cme**2) *
+                     (2 * cggs * (2 * mpi**2 - cme**2) *
                       (-9 * vh - 9 * cffs * vs + 2 * cggs * vs) *
                       (9 * vh + 8 * cggs * vs) +
                       b0 * (mdq + muq) * (9 * vh + 4 * cggs * vs) *
                       (54 * cggs * vh - 32 * cggs**2 * vs + 9 * cffs *
                        (9 * vh + 16 * cggs * vs)))**2) / \
-        ((ms**2 - s)**2 *
+        ((ms**2 - cme**2)**2 *
          (9 * vh + 9 * cffs * vs - 2 * cggs * vs) ** 2 *
          (9 * vh + 4 * cggs * vs)**2 * (9 * vh + 8 * cggs * vs)**2)
 
-    p_i = np.sqrt(s / 4.0 - mx**2)
-    p_f = np.sqrt(s / 4.0 - mpi**2)
+    p_i = np.sqrt(cme**2 / 4.0 - mx**2)
+    p_f = np.sqrt(cme**2 / 4.0 - mpi**2)
 
-    prefactor = 1.0 / (16. * np.pi * s) * p_f / p_i
+    prefactor = 1.0 / (16. * np.pi * cme**2) * p_f / p_i
 
-    return prefactor * unit_matrix_elem_sqrd(np.sqrt(s)) * mat_elem_sqrd
+    return prefactor * unit_matrix_elem_sqrd(np.sqrt(cme**2)) * mat_elem_sqrd
 
 
 def __xx_to_s_pipig(eng_gam, cme, mx, ms, cxxs, cffs, cggs, vs):
+    """
+    Returns the gamma ray energy spectrum for two fermions annihilating into
+    two charged pions and a photon.
 
+    Parameters
+    ----------
+    eng_gam : double
+        Gamma ray energy.
+    cme : double
+        Center of mass energy, or sqrt((ppip + ppim + pg)^2).
+    mx : double
+        Mass of the initial state fermion.
+    ms : double
+        Mass of the scalar mediator.
+    cxxs : double
+        Coupling of the initial state fermion to the scalar mediator.
+    cffs : double
+    cggs : double
+        Coupling of the scalar to gluons.
+    vs : double
+        Vacuum expectation value of the scalar mediator.
+
+    Returns
+    -------
+    Returns gamma ray energy spectrum for
+    :math:`\chi\bar{\chi}\to\pi^{+}\pi^{-}\gamma`.
+    """
     s = E1_to_s(eng_gam, 0.0, cme)
 
     def mat_elem_sqrd(t):
@@ -137,7 +236,7 @@ def __xx_to_s_pipig(eng_gam, cme, mx, ms, cxxs, cffs, cggs, vs):
 
     prefactor1 = phase_space_prefactor(cme)
     prefactor2 = 2 * cme / \
-        __xx_to_s_pipi_xsec(cme**2, mx, ms, cxxs, cffs, cggs, vs)
+        __xx_to_s_pipi_xsec(cme, mx, ms, cxxs, cffs, cggs, vs)
     prefactor3 = cross_section_prefactor(mx, mx, cme)
 
     prefactor = prefactor1 * prefactor2 * prefactor3
@@ -149,7 +248,34 @@ def __xx_to_s_pipig(eng_gam, cme, mx, ms, cxxs, cffs, cggs, vs):
 
 
 def xx_to_s_pipig(eng_gams, cme, mx, ms, cxxs, cffs, cggs, vs):
+    """
+    Returns the gamma ray energy spectrum for two fermions annihilating into
+    two charged pions and a photon.
 
+    Parameters
+    ----------
+    eng_gam : numpy.ndarray or double
+        Gamma ray energy.
+    cme : double
+        Center of mass energy, or sqrt((ppip + ppim + pg)^2).
+    mx : double
+        Mass of the initial state fermion.
+    ms : double
+        Mass of the scalar mediator.
+    cxxs : double
+        Coupling of the initial state fermion to the scalar mediator.
+    cffs : double
+    cggs : double
+        Coupling of the scalar to gluons.
+    vs : double
+        Vacuum expectation value of the scalar mediator.
+
+    Returns
+    -------
+    Returns gamma ray energy spectrum for
+    :math:`\chi\bar{\chi}\to\pi^{+}\pi^{-}\gamma` evaluated at the gamma ray
+    energy(ies).
+    """
     if hasattr(eng_gams, '__len__'):
         return [__xx_to_s_pipig(eng_gam, cme, mx, ms, cxxs, cffs, cggs, vs)
                 for eng_gam in eng_gams]
