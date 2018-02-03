87fd3ad55da411564bb732e402db64054e59ae07
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2018-02-03 14:48:02 -0800

    Updated test modules

--- hazma/test/test_fsr.py
+++ hazma/test/test_fsr.py
@@ -1,6 +1,7 @@
-from hazma.fsr_helper_functions.scalar_mediator_fsr import dnde_s
-from hazma.fsr_helper_functions.pseudo_scalar_mediator_fsr import dnde_p
-from hazma.fsr_helper_functions.vector_mediator_fsr import dnde_v
+from hazma.fsr_helper_functions.scalar_mediator_fsr import dnde_xx_to_s_to_ffg
+from hazma.fsr_helper_functions.pseudo_scalar_mediator_fsr \
+    import dnde_xx_to_p_to_ffg
+from hazma.fsr_helper_functions.vector_mediator_fsr import dnde_xx_to_v_to_ffg
 
 from hazma.parameters import muon_mass as mmu
 
@@ -9,17 +10,17 @@ import numpy as np
 
 def test_scalar_mediator_fsr(cme):
     eng_gams = np.logspace(-3., np.log10(cme), num=150)
-    return dnde_s(eng_gams, cme, mmu)
+    return dnde_xx_to_s_to_ffg(eng_gams, cme, mmu)
 
 
 def test_pseudo_scalar_mediator_fsr(cme):
     eng_gams = np.logspace(-3., np.log10(cme), num=150)
-    return dnde_p(eng_gams, cme, mmu)
+    return dnde_xx_to_p_to_ffg(eng_gams, cme, mmu)
 
 
 def test_vector_mediator_fsr(cme):
     eng_gams = np.logspace(-3., np.log10(cme), num=150)
-    return dnde_v(eng_gams, cme, mmu)
+    return dnde_xx_to_v_to_ffg(eng_gams, cme, mmu)
 
 
 test_scalar_mediator_fsr(1000.)
