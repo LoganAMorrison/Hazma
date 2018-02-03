1724a81a643e0360d8571e079a7e5ff095806431
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2018-02-03 14:58:45 -0800

    Updated test modules

--- hazma/test/test_fsr.py
+++ hazma/test/test_fsr.py
@@ -8,21 +8,19 @@ from hazma.parameters import muon_mass as mmu
 import numpy as np
 
 
-def test_scalar_mediator_fsr(cme):
+def test_scalar_mediator_fsr():
+    cme = 1000.
     eng_gams = np.logspace(-3., np.log10(cme), num=150)
     return dnde_xx_to_s_to_ffg(eng_gams, cme, mmu)
 
 
-def test_pseudo_scalar_mediator_fsr(cme):
+def test_pseudo_scalar_mediator_fsr():
+    cme = 1000.
     eng_gams = np.logspace(-3., np.log10(cme), num=150)
     return dnde_xx_to_p_to_ffg(eng_gams, cme, mmu)
 
 
-def test_vector_mediator_fsr(cme):
+def test_vector_mediator_fsr():
+    cme = 1000.
     eng_gams = np.logspace(-3., np.log10(cme), num=150)
     return dnde_xx_to_v_to_ffg(eng_gams, cme, mmu)
-
-
-test_scalar_mediator_fsr(1000.)
-test_pseudo_scalar_mediator_fsr(1000.)
-test_vector_mediator_fsr(1000.)
