cd23f536da1beb99f681817a3501def423fd68ef
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2018-03-17 10:36:00 -0700

    Cleaned up phase shift functions in unitarization.py

--- hazma/unitarization.py
+++ hazma/unitarization.py
@@ -5,13 +5,13 @@ from .parameters import charged_kaon_mass as mk
 from .parameters import neutral_pion_mass as mpi0
 from .parameters import neutral_kaon_mass as mk0
 from .parameters import fpi
+from cmath import sqrt, log, pi, phase
 
-mPI = (mpi + mpi0) / 2. + 0j
-mK = (mk + mk0) / 2. + 0j
+mPI = (mpi + mpi0) / 2. + 0.0j
+mK = (mk + mk0) / 2. + 0.0j
 FPI = fpi + 0j
 Lam = 1.1 * 10**3 + 0j  # cut-off scale taken to be 1.1 GeV
-q_max = np.sqrt(Lam**2 - mK**2)
-eps = 1.0 * 10**-10
+q_MAX = sqrt(Lam**2 - mK**2)
 
 
 # #######################
@@ -19,112 +19,150 @@ eps = 1.0 * 10**-10
 # #######################
 
 
-def bubble_loop(cme, m):
+def bubble_loop(cme, m, q_max=q_MAX):
     """
     Returns value of bubble loop
 
     Returns
-        G_{11} = i\int_{0}^{\infty}\frac{d^4q}{(2\pi)^4}
-            \frac{1}{q^2-m_{K}^2}\frac{1}{(P-q)^2-m_{K}^2}
+        G_{ii} = i\int_{0}^{\infty}\frac{d^4q}{(2\pi)^4}
+            \frac{1}{q^2-m_{i}^2}\frac{1}{(P-q)^2-m_{i}^2}
+
+    Notes
+    -----
     The integral is regulated with a finite momentum cutoff equal to 1.1 GeV.
     """
-    s = cme**2 + eps * 1j
+    s = complex(cme**2, 0.0)
 
-    return ((np.sqrt(4 * m**2 - s) *
-             np.arctan((np.sqrt(s) * q_max) /
-                       np.sqrt((4 * m**2 - s) * (m**2 + q_max**2)))) /
-            np.sqrt(s) + np.log(m / (q_max + np.sqrt(m**2 + q_max**2)))) / \
-        (8. * np.pi**2)
+    sig = sqrt(1. - 4. * m**2 / s)
+    cut_fac = sqrt(1. + m**2 / q_max**2)
+
+    return (sig * log((sig * cut_fac + 1) / (sig * cut_fac - 1)) -
+            2. * log(q_max / m * (1. + cut_fac))) / 16. / pi**2
 
 
-def __G11(cme):
+def __G11(cme, q_max):
     """
     Returns kaon bubble loop factor.
 
     Returns
+    -------
+    G11 : complex
         G_{11} = i\int_{0}^{\infty}\frac{d^4q}{(2\pi)^4}
             \frac{1}{q^2-m_{K}^2}\frac{1}{(P-q)^2-m_{K}^2}
+
+    Notes
+    -----
     The integral is regulated with a finite momentum cutoff equal to 1.1 GeV.
     """
-    s = cme**2. + 1.0j * eps
+    s = complex(cme**2, 0.0)
 
-    p1 = np.sqrt(s - 4. * mK**2) / 2.
-    p2 = np.sqrt(s - 4. * mPI**2) / 2.
+    p1 = complex(sqrt(s - 4. * mK**2) / 2.)
+    p2 = complex(sqrt(s - 4. * mPI**2) / 2.)
 
-    g11 = bubble_loop(cme, mK)
+    g11 = complex(bubble_loop(cme, mK, q_max))
     return g11
 
-    if np.imag(p1) > 0. and np.imag(p2) > 0.:
+    if p1.imag > 0. and p2.imag > 0.:
         return g11
-    if np.imag(p1) > 0. and np.imag(p2) < 0.:
+    if p1.imag > 0. and p2.imag < 0.:
         return g11
-    if np.imag(p1) < 0. and np.imag(p2) < 0.:
-        return g11 - 2.0j * np.imag(g11)
-    if np.imag(p1) < 0. and np.imag(p2) > 0.:
-        return g11 - 2.0j * np.imag(g11)
+    if p1.imag < 0. and p2.imag < 0.:
+        return g11 - 2.0j * g11.imag
+    if p1.imag < 0. and p2.imag > 0.:
+        return g11 - 2.0j * g11.imag
 
 
-def __G22(cme):
+def __G22(cme, q_max):
     """
     Returns pion bubble loop factor.
 
     Returns
+    -------
+    G22 : complex
         G_{22} = i\int_{0}^{\infty}\frac{d^4q}{(2\pi)^4}
             \frac{1}{q^2-m_{\pi}^2}\frac{1}{(P-q)^2-m_{\pi}^2}
+
+    Notes
+    -----
     The integral is regulated with a finite momentum cutoff equal to 1.1 GeV.
     """
-    s = cme**2. + 1.0j * eps
+    s = complex(cme**2, 0.0)
 
-    p1 = np.sqrt(s - 4. * mK**2) / 2.
-    p2 = np.sqrt(s - 4. * mPI**2) / 2.
+    p1 = complex(sqrt(s - 4. * mK**2) / 2.)
+    p2 = complex(sqrt(s - 4. * mPI**2) / 2.)
 
-    g22 = bubble_loop(cme, mPI)
+    g22 = complex(bubble_loop(cme, mPI, q_max))
 
     return g22
 
-    if np.imag(p1) > 0. and np.imag(p2) > 0.:
+    if p1.imag > 0. and p2.imag > 0.:
         return g22
-    if np.imag(p1) > 0. and np.imag(p2) < 0.:
+    if p1.imag > 0. and p2.imag < 0.:
         return g22
-    if np.imag(p1) < 0. and np.imag(p2) < 0.:
-        return g22 - 2.0j * np.imag(g22)
-    if np.imag(p1) < 0. and np.imag(p2) > 0.:
-        return g22 - 2.0j * np.imag(g22)
+    if p1.imag < 0. and p2.imag < 0.:
+        return g22 - 2.0j * g22.imag
+    if p1.imag < 0. and p2.imag > 0.:
+        return g22 - 2.0j * g22.imag
 
 
 def __v11(cme):
-    s = cme**2. + 1.0j * eps
+    s = complex(cme**2, 0.0)
     return - (1 / 4. / fpi**2) * (3 * s)
 
 
 def __v12(cme):
-    s = cme**2. + 1.0j * eps
-    return - (1. / 3.0 / np.sqrt(12.) / fpi**2) * (9.0 / 2. * s)
+    s = complex(cme**2, 0.0)
+    return - (1. / 3.0 / sqrt(12.) / fpi**2) * (9.0 / 2. * s)
 
 
 def __v22(cme):
-    s = cme**2. + 1.0j * eps
+    s = complex(cme**2, 0.0)
     return - (1. / 9. / fpi**2) * (9. * s + 15. * mPI**2 / 2. - 12. * mPI**2)
 
 
-def __delta_pi(cme):
-    return 1.0 - __v22(cme) * __G22(cme)
+def __delta_pi(cme, q_max):
+    return 1.0 - __v22(cme) * __G22(cme, q_max)
 
 
-def __delta_k(cme):
-    return 1.0 - __v11(cme) * __G11(cme)
+def __delta_k(cme, q_max):
+    return 1.0 - __v11(cme) * __G11(cme, q_max)
 
 
-def __delta_c(cme):
-    return __delta_pi(cme) * __delta_k(cme) - \
-        __v12(cme)**2 * __G11(cme) * __G22(cme)
+def __delta_c(cme, q_max):
+    return __delta_pi(cme, q_max) * __delta_k(cme, q_max) - \
+        __v12(cme)**2 * __G11(cme, q_max) * __G22(cme, q_max)
 
 
-def __delta(cme):
-    return __delta_pi(cme) * __delta_c(cme)
+def __delta(cme, q_max):
+    return __delta_pi(cme, q_max) * __delta_c(cme, q_max)
 
 
-def amp_bethe_salpeter_kk_to_kk(cme):
+def __amp_bs_kk_to_kk(cme, q_max):
+
+    Q = complex(cme)
+
+    return complex((__delta_pi(Q, q_max) * __v11(Q) +
+                    __v12(Q)**2 * __G22(Q, q_max)) / __delta_c(Q, q_max))
+
+
+def __amp_bs_pipi_to_kk(cme, q_max):
+
+    Q = complex(cme)
+
+    return complex((__v12(Q) * __G11(Q, q_max) * __v11(Q) +
+                    __delta_k(Q, q_max) * __v12(Q)) / __delta_c(Q, q_max))
+
+
+def __amp_bs_pipi_to_pipi(cme, q_max):
+
+    Q = complex(cme)
+
+    return complex(__v22(Q) / __delta_pi(Q, q_max) +
+                   __v12(Q)**2 * __G11(Q, q_max) /
+                   (__delta_pi(Q, q_max) * __delta_c(Q, q_max)))
+
+
+def amp_bethe_salpeter_kk_to_kk(cmes, q_max=q_MAX):
     """
     Returns the unitarized matrix element for kk -> kk in the zero isospin
     channel.
@@ -135,22 +173,23 @@ def amp_bethe_salpeter_kk_to_kk(cme):
 
     Parameters
     ----------
-    Q: float
-        Invariant mass of the pions.
+    cmes: array-like or float
+        Invariant mass of the kaons.
 
     Returns
     -------
-    Mu: float
+    Mu: array-like or float
         Unitarized matrix element for kk -> kk in the zero isospin
         channel.
     """
 
-    Q = cme + 0.0j
-    return (__delta_pi(Q) * __v11(Q) +
-            __v12(Q)**2 * __G22(Q)) / __delta_c(Q) + 0.0j
+    if hasattr(cmes, "__len__"):
+        return np.array([__amp_bs_kk_to_kk(cme, q_max) for cme in cmes])
+
+    return __amp_bs_kk_to_kk(cmes, q_max)
 
 
-def amp_bethe_salpeter_pipi_to_kk(cme):
+def amp_bethe_salpeter_pipi_to_kk(cmes, q_max=q_MAX):
     """
     Returns the unitarized matrix element for pipi -> kk in the zero isospin
     channel.
@@ -161,22 +200,23 @@ def amp_bethe_salpeter_pipi_to_kk(cme):
 
     Parameters
     ----------
-    Q: float
-        Invariant mass of the pions.
+    cmes: array-like or float
+        Invariant mass of the kaons.
 
     Returns
     -------
-    Mu: float
+    Mu: array-like or float
         Unitarized matrix element for pipi -> kk in the zero isospin
         channel.
     """
 
-    Q = cme + 0.0j
-    return (__v12(Q) * __G11(Q) * __v11(Q) +
-            __delta_k(Q) * __v12(Q)) / __delta_c(Q) + 0.0j
+    if hasattr(cmes, "__len__"):
+        return np.array([__amp_bs_pipi_to_kk(cme, q_max) for cme in cmes])
+
+    return __amp_bs_pipi_to_kk(cmes, q_max)
 
 
-def amp_bethe_salpeter_pipi_to_pipi(cme):
+def amp_bethe_salpeter_pipi_to_pipi(cmes, q_max=q_MAX):
     """
     Returns the unitarized matrix element for pipi -> pipi in the zero isospin
     channel.
@@ -187,52 +227,104 @@ def amp_bethe_salpeter_pipi_to_pipi(cme):
 
     Parameters
     ----------
-    Q: float
+    cmes: array-like or float
         Invariant mass of the pions.
 
     Returns
     -------
-    Mu: float
+    Mu: complex array-like or complex
         Unitarized matrix element for pipi -> pipi in the zero isospin
         channel.
     """
 
-    Q = cme + 0.0j
-    return __v22(Q) / __delta_pi(Q) + \
-        __v12(Q)**2 * __G11(Q) / (__delta_pi(Q) * __delta_c(Q)) + 0.0j
+    if hasattr(cmes, "__len__"):
+        return np.array([__amp_bs_pipi_to_pipi(cme, q_max) for cme in cmes])
+
+    return __amp_bs_pipi_to_pipi(cmes, q_max)
 
 
-def __phase_shift_pipi_to_pipi(cme, deg=False):
-    s = cme**2. + 1.0j * eps
-    p2 = np.sqrt(s - 4 * mPI**2) / 2.
+def __phase_shift(cme, t, deg=False):
+    s = complex(cme**2, 0.0)
 
-    z = 1.0 - 2.0j * p2 * \
-        amp_bethe_salpeter_pipi_to_pipi(np.sqrt(s)) / (8 * np.pi * np.sqrt(s))
+    p2 = complex(sqrt(s - 4 * mPI**2) / 2.)
 
-    x, y = np.real(z), np.imag(z)
-    theta = np.abs(np.arctan(np.abs(y / x)))
+    z = complex(1.0 - 2.0j * p2 * t / (8. * pi * sqrt(s)))
 
-    if x > 0. and y > 0.:
-        theta = theta
-    if x < 0. and y > 0.:
-        theta = np.pi - theta
-    if x < 0. and y < 0.:
-        theta = np.pi + theta
-    if x > 0. and y < 0.:
-        theta = 2. * np.pi - theta
+    theta = phase(z)
+
+    if z.imag < 0.:
+        theta = theta + 2. * pi
 
     if deg is True:
-        theta = theta * 180. / np.pi
+        theta = theta * 180. / pi
 
     return theta / 2.0
 
 
-def phase_shift_pipi_to_pipi(cmes, deg=False):
-    if hasattr(cmes, '__len__'):
-        return np.array([__phase_shift_pipi_to_pipi(cme, deg=deg)
-                         for cme in cmes])
+def phase_shift(cme, t, deg=False):
+    """
+    Computes the phase shift of an amplitude.
+
+    Parameters
+    ----------
+    cme : float
+        Center of mass energy of process.
+    t : complex
+        The amplitude of the process
+
+    Returns
+    -------
+    delta : float
+        Phase shift.
+    """
+    if hasattr(cme, '__len__'):
+        return np.array([__phase_shift(cme[i], t[i], deg=deg)
+                         for i in range(len(cme))])
     else:
-        return __phase_shift_pipi_to_pipi(cmes, deg=deg)
+        return __phase_shift(cme, t, deg=deg)
+
+
+def __find_branch_index(angles, trigger=250):
+    """
+    Returns index where phases encounter a branch cut.
+    """
+    angles2 = 2 * angles
+    index = 1
+
+    while True:
+        angle1 = angles2[index]
+        angle2 = angles2[index - 1]
+        if abs(angle1 - angle2) > trigger:
+            break
+        elif (index + 1 == len(angles)):
+            index = None
+            break
+        else:
+            index = index + 1
+
+    return index
+
+
+def fix_phases(phases, trigger=250):
+    """
+    Returns an corrected phases taking into account branch cuts.
+
+    Parameters
+    ----------
+    phases : array-like
+        List of phases.
+    trigger : float, optional {250}
+        Phase jump to trigger on.
+
+    Returns
+    -------
+    phases : array-like
+        Corrected list of phases.
+    """
+    index = __find_branch_index(phases, trigger)
+    if index is not None:
+        phases[index::] = phases[index::] + 180
+    return phases
 
 
 # ########################
@@ -260,7 +352,7 @@ def amp_pipi_to_pipi_I0(cme):
     cme: double
         Invariant mass of the two charged pions.
     """
-    return (2 * cme**2 - mpi0**2) / (32. * fpi**2 * np.pi)
+    return (2 * cme**2 - mpi0**2) / (32. * fpi**2 * pi)
 
 
 def amp_inverse_amplitude_pipi_to_pipi(cme):
