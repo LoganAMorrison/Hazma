6b3bd464bcdc0ea8a46c5bd71756632643cca89d
Adam Coogan <acoogan@ucsc.edu>
2018-04-16 15:33:19 -0700

    Refactored gamma ray background. Made containers for experiment and target information.

--- hazma/gamma_ray_limits/gamma_ray_limit_parameters.py
+++ hazma/gamma_ray_limits/gamma_ray_limit_parameters.py
@@ -1,9 +1,17 @@
 """
 Parameters relevant to computing constraints from gamma ray experiments
 """
+from collections import namedtuple
+import numpy as np
+from scipy.interpolate import interp1d
 
 
-# Gamma ray experiment parameters
+# Background model from arxiv:1703.02546
+e_Bs, dPhi_dEdOmega_Bs = np.transpose(np.loadtxt("background_1703-02546.csv",
+                                                 delimiter=","))
+dPhi_dEdOmega_Bs = 1.0e3 * dPhi_dEdOmega_Bs / e_Bs**2  # convert GeV^2 -> MeV^2
+dPhi_dEdOmega_B_default = interp1d(e_Bs, dPhi_dEdOmega_Bs)
+
 # Reference angular size of dSph galaxy
 delta_Omega_dSph = 1.0e-3  # sr
 # Approximate effective area of e-ASTROGAM
@@ -12,3 +20,15 @@ A_eff_e_ASTROGAM = 500.  # cm^2
 T_obs_e_ASTROGAM = 365. * 24. * 60.**2  # s
 # J factors for various objects, all in MeV^2 cm^-5
 J_factor_draco = 1.0e29
+
+# Object to hold gamma ray parameters
+ExperimentParams = namedtuple("gamma_ray_exp_params", ["A_eff", "T_obs"])
+# e-ASTROGAM's parameters
+eASTROGAM_params = ExperimentParams(A_eff_e_ASTROGAM, T_obs_e_ASTROGAM)
+
+# Object to source information
+TargetParams = namedtuple("gamma_ray_target_params",
+                          ["dPhi_dEdOmega_B", "J_factor", "delta_Omega"])
+# Generic dSph parameters
+dSph_params = TargetParams(dPhi_dEdOmega_B_default, J_factor_draco,
+                           delta_Omega_dSph)
