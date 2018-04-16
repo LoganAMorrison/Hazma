e6b68369da6dafc60492c843aa0df3a930f71416
Adam Coogan <acoogan@ucsc.edu>
2018-04-16 14:23:42 -0700

    Moved gamma ray experiment parameters to their own file

---
+++ hazma/gamma_ray_limits/gamma_ray_limit_parameters.py
@@ -0,0 +1,14 @@
+"""
+Parameters relevant to computing constraints from gamma ray experiments
+"""
+
+
+# Gamma ray experiment parameters
+# Reference angular size of dSph galaxy
+delta_Omega_dSph = 1.0e-3  # sr
+# Approximate effective area of e-ASTROGAM
+A_eff_e_ASTROGAM = 500.  # cm^2
+# Approximate observing time for e-ASTROGAM
+T_obs_e_ASTROGAM = 365. * 24. * 60.**2  # s
+# J factors for various objects, all in MeV^2 cm^-5
+J_factor_draco = 1.0e29
--- hazma/parameters.py
+++ hazma/parameters.py
@@ -74,13 +74,3 @@ Vus = 0.2248
 
 # widths
 rho_width = 146.2
-
-# Gamma ray experiment parameters
-# Reference angular size of dSph galaxy
-delta_Omega_dSph = 1.0e-3  # sr
-# Approximate effective area of e-ASTROGAM
-A_eff_e_ASTROGAM = 500.  # cm^2
-# Approximate observing time for e-ASTROGAM
-T_obs_e_ASTROGAM = 365. * 24. * 60.**2  # s
-# J factors for various objects, all in MeV^2 cm^-5
-J_factor_draco = 1.0e29
