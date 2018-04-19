a48f6588c4c69e7d9f9a5ab406ced5c3628bf47b
Adam Coogan <acoogan@ucsc.edu>
2018-04-19 12:49:29 -0700

    Modified <sigma v> limit computation to choose optimal energy window. Currently runs slowly.

--- hazma/gamma_ray_limits/compute_limits.py
+++ hazma/gamma_ray_limits/compute_limits.py
@@ -1,12 +1,13 @@
-from gamma_ray_limit_parameters import (ExperimentParams, TargetParams,
-                                        eASTROGAM_params, dSph_params)
+from gamma_ray_limit_parameters import eASTROGAM_params, dSph_params
+from ..parameters import neutral_pion_mass as mpi0
+from scipy.interpolate import interp1d
+from scipy import optimize
 from scipy.integrate import quad
 import numpy as np
 
 
-def compute_limit(dN_dE_DM, mx, e_gam_min, e_gam_max, self_conjugate=False,
-                  n_sigma=5., exp_params=eASTROGAM_params,
-                  target_params=dSph_params):
+def compute_limit(dN_dE_DM, mx, self_conjugate=False, n_sigma=5.,
+                  exp_params=eASTROGAM_params, target_params=dSph_params):
     """Computes smallest value of <sigma v> detectable for given target and
     experiment parameters.
 
@@ -30,10 +31,6 @@ def compute_limit(dN_dE_DM, mx, e_gam_min, e_gam_max, self_conjugate=False,
         energy
     mx : float
         Dark matter mass
-    e_gam_min : float
-        Lower bound for energy window used to set limit
-    e_gam_max : float
-        Upper bound for energy window used to set limit
     dPhi_dEdOmega_B : float -> float
         Background photon spectrum per solid angle as a function of photon
         energy
@@ -53,17 +50,41 @@ def compute_limit(dN_dE_DM, mx, e_gam_min, e_gam_max, self_conjugate=False,
 
     Returns
     -------
-    <sigma v> : float
+    <sigma v>_tot : float
         Smallest detectable thermally averaged total cross section in cm^3 / s
     """
-    # Prefactor for converting integrated spectrum to photon counts
-    prefactor = exp_params.T_obs * target_params.delta_Omega
+    # Initial guesses for energy window bounds
+    e_a_0 = mpi0 / 2.
+    e_b_0 = mx
 
-    # Number of background photons
-    def integrand_B(e_gam):
-        return (prefactor * target_params.dPhi_dEdOmega_B(e_gam) *
-                exp_params.A_eff(e_gam))
-    N_gam_B = quad(integrand_B, e_gam_min, e_gam_max)[0]
+    # Bounds on upper and lower limits for energy window
+    e_a_bounds = [dN_dE_DM.x[0], mpi0 / 2.00001]
+    e_b_bounds = [mpi0 / 1.99999, mx]
+
+    # Integrand required to compute number of photons from DM annihilations
+    def I_S(e_a, e_b):
+        def integrand_S(e):
+            return dN_dE_DM(e) * exp_params.A_eff(e)
+
+        return quad(integrand_S, e_a, e_b)[0]
+
+    # Integrand required to compute number of background photons
+    def I_B(e_a, e_b):
+        def integrand_B(e):
+            return target_params.dPhi_dEdOmega_B(e) * exp_params.A_eff(e)
+
+        return quad(integrand_B, e_a, e_b)[0]
+
+    # Function to optimize to select energy window
+    def objective(e_ab):
+        e_a = e_ab[0]
+        e_b = e_ab[1]
+
+        return -I_S(e_a, e_b) / np.sqrt(I_B(e_a, e_b))
+
+    # Minimize the objective function to compute the limit
+    limit_obj = optimize.minimize(objective, [e_a_0, e_b_0],
+                                  bounds=[e_a_bounds, e_b_bounds])
 
     # Factor to avoid double counting pairs of DM particles
     if self_conjugate:
@@ -71,12 +92,9 @@ def compute_limit(dN_dE_DM, mx, e_gam_min, e_gam_max, self_conjugate=False,
     else:
         dm_factor = 4.
 
-    # Number of signal photons
-    def integrand_S(e_gam):
-        dm_prefactor = prefactor * target_params.J_factor / (4. * np.pi *
-                                                             dm_factor * mx**2)
-        return dm_prefactor * dN_dE_DM(e_gam) * exp_params.A_eff(e_gam)
+    # Insert appropriate prefactors to convert result to <sigma v>_tot
+    prefactor = 4. * np.pi * dm_factor * mx**2 / \
+        (np.sqrt(exp_params.T_obs * target_params.delta_Omega) *
+         target_params.J_factor)
 
-    N_gam_S = quad(integrand_S, e_gam_min, e_gam_max)[0]
-
-    return n_sigma * np.sqrt(N_gam_B) / N_gam_S
+    return prefactor * n_sigma / (-limit_obj.fun)
--- hazma/theory.py
+++ hazma/theory.py
@@ -36,26 +36,6 @@ class Theory(object):
     def spectrum_functions(self):
         pass
 
-    def get_e_gam_window(self):
-        """Return best gamma ray energy range over which to probe this model.
-
-        Notes
-        -----
-        This could probably be significantly optimized. For a start, centering
-        it on the pi0 peak would be a big improvement...
-
-        Returns
-        -------
-        e_gam_min, e_gam_max : (float, float)
-            Boundaries of energy window
-        """
-        if self.mx < mpi0:
-            e_gam_min = 5.
-        else:
-            e_gam_min = mpi0 / 2.
-
-        return e_gam_min, self.mx
-
     def compute_limit(self, n_sigma=5., exp_params=eASTROGAM_params,
                       target_params=dSph_params):
         """Computes smallest value of <sigma v> detectable for given target and
@@ -82,10 +62,6 @@ class Theory(object):
             photon energy
         mx : float
             Dark matter mass
-        e_gam_min : float
-            Lower bound for energy window used to set limit
-        e_gam_max : float
-            Upper bound for energy window used to set limit
         dPhi_dEdOmega_B : float -> float
             Background photon spectrum per solid angle as a function of photon
             energy
@@ -109,16 +85,13 @@ class Theory(object):
             Smallest detectable thermally averaged total cross section in units
             of cm^3 / s
         """
-        # Choose energy window in which to search for signal
-        e_gam_min, e_gam_max = self.get_e_gam_window()
-
         # Create function to interpolate spectrum over energy window
-        e_gams = np.logspace(np.log10(e_gam_min), np.log10(e_gam_max), 100)
+        e_gams = np.logspace(0., np.log10(self.mx), 200)
         dN_dE_DM = interp1d(e_gams,
                             self.spectra(e_gams, 2.001*self.mx)["total"])
 
-        return compute_limit(dN_dE_DM, self.mx, e_gam_min, e_gam_max,
-                             n_sigma=n_sigma, exp_params=exp_params,
+        return compute_limit(dN_dE_DM, self.mx, n_sigma=n_sigma,
+                             exp_params=exp_params,
                              target_params=target_params)
 
     def compute_limits(self, mxs, n_sigma=5., exp_params=eASTROGAM_params,
