bb36e777a1e733af3f994cb3a9efadddb81f30b5
Adam Coogan <acoogan@ucsc.edu>
2018-05-01 19:33:34 -0700

    Put in cross section for x x -> v -> pi0 pi pi

--- hazma/vector_mediator/vector_mediator_cross_sections.py
+++ hazma/vector_mediator/vector_mediator_cross_sections.py
@@ -1,5 +1,3 @@
-# from ..parameters import muon_mass as mmu
-# from ..parameters import electron_mass as me
 from cmath import sqrt, pi
 from ..parameters import charged_pion_mass as mpi
 from ..parameters import neutral_pion_mass as mpi0
@@ -7,6 +5,7 @@ from ..parameters import fpi
 from ..parameters import qe
 from ..parameters import muon_mass as mmu
 from ..parameters import electron_mass as me
+from scipy.integrate import quad
 
 
 def sigma_xx_to_v_to_ff(Q, f, params):
@@ -107,6 +106,44 @@ def sigma_xx_to_v_to_pi0g(Q, params):
         return 0.
 
 
+def __sigma_t_integrated_xx_to_v_to_pi0pipi(s, Q, params):
+    gvuu = params.gvuu
+    gvdd = params.gvdd
+    gvxx = params.gvxx
+    mx = params.mx
+    mv = params.mv
+
+    if (Q > 2. * mpi + mpi0 and Q > 2. * mx and s > 4.*mpi**2
+            and s < (Q - mpi0)**2):
+        ret_val = ((gvdd + gvuu)**2*gvxx**2*sqrt(s*(-4*mpi**2 + s)) *
+                   sqrt(Q**4 + (mpi0**2 - s)**2 - 2*Q**2*(mpi0**2 + s)) *
+                   (-24*mpi**6*s + mpi**4*(-2*mpi0**4 + 28*mpi0**2*s +
+                                           22*s**2) +
+                    2*mpi**2*(mpi0**6 - 4*s**3) +
+                    s*(-2*mpi0**6 - 4*mpi0**4*s - mpi0**2*s**2 + s**3) +
+                    Q**4*(-2*mpi**4 + 2*mpi**2*(mpi0**2 - s) +
+                          s*(-2*mpi0**2 + s)) +
+                    Q**2*(4*mpi**4*(mpi0**2 + s) +
+                          s*(4*mpi0**4 + 5*mpi0**2*s - 2*s**2) -
+                          4*mpi**2*(mpi0**4 + 3*mpi0**2*s - s**2)))) / \
+            (294912.*fpi**6*pi**7*sqrt(Q**2)*(-mv**2 + Q**2)**2 *
+             sqrt(-4*mx**2 + Q**2)*s**2)
+
+        assert ret_val.imag == 0.
+
+        return ret_val.real
+    else:
+        return 0.
+
+
+def sigma_xx_to_v_to_pi0pipi(Q, params):
+    s_min = 4. * mpi**2
+    s_max = (Q - mpi0)**2
+
+    return quad(__sigma_t_integrated_xx_to_v_to_pi0pipi, s_min, s_max,
+                args=(Q, params))[0]
+
+
 def cross_sections(Q, params):
     """
     Compute the total cross section for two fermions annihilating through a
@@ -126,13 +163,16 @@ def cross_sections(Q, params):
     electron_contr = sigma_xx_to_v_to_ff(Q, "e", params)
     pipi_contr = sigma_xx_to_v_to_pipi(Q, params)
     pi0g_contr = sigma_xx_to_v_to_pi0g(Q, params)
+    pi0pipi_contr = sigma_xx_to_v_to_pi0pipi(Q, params)
 
-    total = muon_contr + electron_contr + pipi_contr + pi0g_contr
+    total = (muon_contr + electron_contr + pipi_contr + pi0g_contr +
+             pi0pipi_contr)
 
     cross_secs = {'mu mu': muon_contr,
                   'e e': electron_contr,
                   'pi pi': pipi_contr,
                   'pi0 g': pi0g_contr,
+                  'pi0 pi pi': pi0pipi_contr,
                   'total': total}
 
     return cross_secs
@@ -160,9 +200,11 @@ def branching_fractions(Q, params):
         return {'mu mu': 0.0,
                 'e e': 0.0,
                 'pi pi': 0.0,
-                'pi0 g': 0.0}
+                'pi0 g': 0.0,
+                "pi0 pi pi": 0.0}
     else:
         return {'mu mu': CSs['mu mu'] / CSs['total'],
                 'e e': CSs['e e'] / CSs['total'],
                 'pi pi': CSs['pi pi'] / CSs['total'],
-                'pi0 g': CSs['pi0 g'] / CSs['total']}
+                'pi0 g': CSs['pi0 g'] / CSs['total'],
+                "pi0 pi pi": CSs["pi0 pi pi"] / CSs["total"]}
