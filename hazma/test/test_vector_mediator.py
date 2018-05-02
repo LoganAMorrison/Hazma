b0f7916f2bf6b59d083d5c1dc266ffcc7364378c
Adam Coogan <acoogan@ucsc.edu>
2018-05-01 19:37:01 -0700

    Fixed vector mediator tests, which called an outdated constructor for that theory.

--- hazma/test/test_vector_mediator.py
+++ hazma/test/test_vector_mediator.py
@@ -4,45 +4,45 @@ import numpy as np
 
 def test_list_final_states():
     mx, mv = [100., 200.]
-    gvxx, gvff = [1., 1.]
+    gvxx, gvuu, gvdd, gvss, gvee, gvmumu = 6 * [1.]
 
-    VM = VectorMediator(mx, mv, gvxx, gvff)
+    VM = VectorMediator(mx, mv, gvxx, gvxx, gvuu, gvdd, gvss, gvee, gvmumu)
     VM.list_final_states()
 
 
 def test_cross_sections():
     mx, mv = [100., 200.]
-    gvxx, gvff = [1., 1.]
     cme = 1000.
+    gvxx, gvuu, gvdd, gvss, gvee, gvmumu = 6 * [1.]
 
-    VM = VectorMediator(mx, mv, gvxx, gvff)
+    VM = VectorMediator(mx, mv, gvxx, gvxx, gvuu, gvdd, gvss, gvee, gvmumu)
     VM.cross_sections(cme)
 
 
 def test_branching_fractions():
     mx, mv = [100., 200.]
-    gvxx, gvff = [1., 1.]
     cme = 1000.
+    gvxx, gvuu, gvdd, gvss, gvee, gvmumu = 6 * [1.]
 
-    VM = VectorMediator(mx, mv, gvxx, gvff)
+    VM = VectorMediator(mx, mv, gvxx, gvxx, gvuu, gvdd, gvss, gvee, gvmumu)
     VM.branching_fractions(cme)
 
 
 def test_spectra():
     mx, mv = [100., 200.]
-    gvxx, gvff = [1., 1.]
     cme = 1000.
     egams = np.logspace(0., np.log10(cme), num=50)
+    gvxx, gvuu, gvdd, gvss, gvee, gvmumu = 6 * [1.]
 
-    VM = VectorMediator(mx, mv, gvxx, gvff)
+    VM = VectorMediator(mx, mv, gvxx, gvxx, gvuu, gvdd, gvss, gvee, gvmumu)
     VM.spectra(egams, cme)
 
 
 def test_spectrum_functions():
     mx, mv = [100., 200.]
-    gvxx, gvff = [1., 1.]
+    gvxx, gvuu, gvdd, gvss, gvee, gvmumu = 6 * [1.]
 
-    VM = VectorMediator(mx, mv, gvxx, gvff)
+    VM = VectorMediator(mx, mv, gvxx, gvxx, gvuu, gvdd, gvss, gvee, gvmumu)
     VM.spectrum_functions()
 
 
