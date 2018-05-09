6c90ba0a3ffb537376a6318258c3f5c80e7b62d4
Adam Coogan <acoogan@ucsc.edu>
2018-05-09 11:19:30 -0700

    Added 10deg x 10deg of the Galactic Center as a gamma ray target

--- hazma/gamma_ray_limits/gamma_ray_limit_parameters.py
+++ hazma/gamma_ray_limits/gamma_ray_limit_parameters.py
@@ -27,6 +27,26 @@ fermi_energy_res_rf = resource_filename(__name__,
                                         "fermi_energy_resolution.dat")
 e_astrogam_energy_res_rf = resource_filename(__name__,
                                              "e-astrogam_energy_resolution.dat")
+gc_bg_model_rf = resource_filename(__name__, "gc_bg_model.dat")
+
+
+def load_interp(rf_name, bounds_error=False, fill_value=0.0):
+    """Creates an interpolator from a data file.
+
+    Parameters
+    ----------
+    rf_name : resource_filename
+        Name of resource file.
+
+    Returns
+    -------
+    interp : interp1d
+        An interpolator created using the first column of the file as the x
+        values and second as the y values. interp will not raise a bounds error
+        and uses a fill values of 0.0.
+    """
+    xs, ys = np.loadtxt(rf_name, delimiter=",").T
+    return interp1d(xs, ys, bounds_error=bounds_error, fill_value=fill_value)
 
 
 class BackgroundModel(object):
@@ -114,55 +134,6 @@ class BackgroundModel(object):
                 return np.array([np.nan])
 
 
-# This is the background model from arXiv:1504.04024, eq. 14. It was derived
-# by performing a simple power law fit to COMPTEL data from 0.8 - 30 MeV and
-# EGRET data from 30 MeV - 10 GeV. We take the lower range of validity to be
-# the lowest energy for which e-ASTROGAM has nonzero effective area.
-default_bg_model = BackgroundModel([0.3, 10.0e3], lambda e: 2.74e-3 / e**2)
-
-def load_interp(rf_name, bounds_error=False, fill_value=0.0):
-    """Creates an interpolator from a data file.
-
-    Parameters
-    ----------
-    rf_name : resource_filename
-        Name of resource file.
-
-    Returns
-    -------
-    interp : interp1d
-        An interpolator created using the first column of the file as the x
-        values and second as the y values. interp will not raise a bounds error
-        and uses a fill values of 0.0.
-    """
-    xs, ys = np.transpose(np.loadtxt(rf_name, delimiter=","))
-    return interp1d(xs, ys, bounds_error=bounds_error, fill_value=fill_value)
-
-
-# Effective areas in cm^2
-A_eff_e_astrogam = load_interp(A_eff_e_astrogam_rf)
-A_eff_fermi = load_interp(A_eff_fermi_rf)
-A_eff_comptel = load_interp(A_eff_comptel_rf)
-A_eff_egret = load_interp(A_eff_egret_rf)
-
-# Energy resolutions, Delta E / E
-energy_res_comptel = load_interp(comptel_energy_res_rf,
-                                 fill_value="extrapolate")
-energy_res_egret = load_interp(egret_energy_res_rf, fill_value="extrapolate")
-energy_res_fermi = load_interp(fermi_energy_res_rf, fill_value="extrapolate")
-energy_res_e_astrogam = load_interp(e_astrogam_energy_res_rf,
-                                    fill_value="extrapolate")
-
-# Approximate observing time for e-ASTROGAM in seconds
-T_obs_e_astrogam = 365. * 24. * 60.**2
-
-
-# Angular sizes (in sr) and J factors (in MeV^2 cm^-5) for various objects
-TargetParams = namedtuple("TargetParams", ["J", "dOmega"])
-# Dwarf with high J factor
-draco_params = TargetParams(1.0e29, 1.0e-3)
-
-
 def solid_angle(l_max, b_min, b_max):
     """Returns solid angle subtended for a target region.
 
@@ -184,6 +155,39 @@ def solid_angle(l_max, b_min, b_max):
     return 4. * l_max*deg_to_rad * (np.sin(b_max*deg_to_rad) -
                                     np.sin(b_min*deg_to_rad))
 
+
+# Angular sizes (in sr) and J factors (in MeV^2 cm^-5) for various objects
+TargetParams = namedtuple("TargetParams", ["J", "dOmega"])
+# Dwarf with high J factor
+draco_params = TargetParams(1.0e29, 1.0e-3)
+
+# This is the background model from arXiv:1504.04024, eq. 14. It was derived
+# by performing a simple power law fit to COMPTEL data from 0.8 - 30 MeV and
+# EGRET data from 30 MeV - 10 GeV. We take the lower range of validity to be
+# the lowest energy for which e-ASTROGAM has nonzero effective area.
+default_bg_model = BackgroundModel([0.3, 10.0e3], lambda e: 2.74e-3 / e**2)
+
+# This is the more complex background model from arXiv:1703.02546. Note that it
+# is only applicable to the inner 10deg x 10deg region of the Milky Way.
+gc_bg_model = BackgroundModel.from_file(gc_bg_model_rf)
+gc_bg_model_target = TargetParams(1.795e29, solid_angle(10., 0., 10.))
+
+# Effective areas in cm^2
+A_eff_e_astrogam = load_interp(A_eff_e_astrogam_rf)
+A_eff_fermi = load_interp(A_eff_fermi_rf)
+A_eff_comptel = load_interp(A_eff_comptel_rf)
+A_eff_egret = load_interp(A_eff_egret_rf)
+
+# Energy resolutions, Delta E / E
+energy_res_comptel = load_interp(comptel_energy_res_rf,
+                                 fill_value="extrapolate")
+energy_res_egret = load_interp(egret_energy_res_rf, fill_value="extrapolate")
+energy_res_fermi = load_interp(fermi_energy_res_rf, fill_value="extrapolate")
+energy_res_e_astrogam = load_interp(e_astrogam_energy_res_rf,
+                                    fill_value="extrapolate")
+
+# Approximate observing time for e-ASTROGAM in seconds
+T_obs_e_astrogam = 365. * 24. * 60.**2
 
 class FluxMeasurement(object):
     """Container for all information about a completed gamma ray analysis.
