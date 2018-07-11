cbfe8d987b50da9de23e70f67ecfccfdd2498048
Adam Coogan <acoogan@ucsc.edu>
2018-07-11 10:03:49 -0700

    Put in all pseudoscalar spectra except FSR for pi0 pi pi.

--- hazma/pseudo_scalar_mediator/pseudo_scalar_mediator_spectra.py
+++ hazma/pseudo_scalar_mediator/pseudo_scalar_mediator_spectra.py
@@ -3,46 +3,54 @@ import numpy as np
 from .pseudo_scalar_mediator_fsr import dnde_xx_to_p_to_ffg
 from .pseudo_scalar_mediator_cross_sections import branching_fractions
 
-from ..decay import muon
+from ..decay import muon, charged_pion, neutral_pion
 
 from ..parameters import muon_mass as mmu
 from ..parameters import electron_mass as me
 
 
-def dnde_ee(egams, cme, params, type='All'):
-    fsr = np.vectorize(dnde_xx_to_p_to_ffg)
-
-    if type == 'All':
-        return fsr(egams, cme / 2., me, params)
-    if type == 'FSR':
-        return fsr(egams, cme / 2., me, params)
-    if type == 'Decay':
-        return np.arrary([0.0 for _ in range(len(egams))])
+def dnde_ee(egams, cme, params, spectrum_type='All'):
+    if spectrum_type == 'All':
+        return (dnde_ee(egams, cme, params, 'FSR') +
+                dnde_ee(egams, cme, params, 'Decay'))
+    elif spectrum_type == 'FSR':
+        return dnde_xx_to_p_to_ffg(egams, cme, me, params)
+    elif spectrum_type == 'Decay':
+        return np.array([0.0 for _ in range(len(egams))])
     else:
         raise ValueError("Type {} is invalid. Use 'All', 'FSR' or \
-                         'Decay'".format(type))
+                         'Decay'".format(spectrum_type))
 
 
-def dnde_mumu(egams, cme, params, type='All'):
-    fsr = np.vectorize(dnde_xx_to_p_to_ffg)
-    decay = np.vectorize(muon)
-
-    if type == 'All':
-        mu_decay = decay(egams, cme / 2.0)
-        mu_fsr = fsr(egams, cme / 2., mmu, params)
-        return 2. * mu_decay + mu_fsr
-    if type == 'FSR':
-        return fsr(egams, cme / 2., mmu, params)
-    if type == 'Decay':
-        return 2. * decay(egams, cme / 2.0)
+def dnde_mumu(egams, cme, params, spectrum_type='All'):
+    if spectrum_type == 'All':
+        return (dnde_mumu(egams, cme, params, 'FSR') +
+                dnde_mumu(egams, cme, params, 'Decay'))
+    elif spectrum_type == 'FSR':
+        return dnde_xx_to_p_to_ffg(egams, cme, mmu, params)
+    elif spectrum_type == 'Decay':
+        return 2. * muon(egams, cme / 2.0)
     else:
         raise ValueError("Type {} is invalid. Use 'All', 'FSR' or \
-                         'Decay'".format(type))
+                         'Decay'".format(spectrum_type))
+
+
+def dnde_pi0pipi(egams, cme, params, spectrum_type='All'):
+    if spectrum_type == 'All':
+        return (dnde_pi0pipi(egams, cme, params, 'FSR') +
+                dnde_pi0pipi(egams, cme, params, 'Decay'))
+    elif spectrum_type == 'FSR':
+        # TODO: implement this using Low's theorem or something
+        return np.array([0.0 for _ in range(len(egams))])
+    elif spectrum_type == 'Decay':
+        return 2. * charged_pion(egams, cme / 2.0) + neutral_pion
+    else:
+        raise ValueError("Type {} is invalid. Use 'All', 'FSR' or \
+                         'Decay'".format(spectrum_type))
 
 
 def spectra(egams, cme, params):
-    """
-    Compute the total spectrum from two fermions annihilating through a
+    """Compute the total spectrum from two fermions annihilating through a
     scalar mediator to mesons and leptons.
 
     Parameters
@@ -58,7 +66,6 @@ def spectra(egams, cme, params):
         Dictionary of the spectra. The keys are 'total', 'mu mu', 'e e',
         'pi0 pi0', 'pi pi', 'k k', 'k0 k0'.
     """
-
     # Compute branching fractions
     bfs = branching_fractions(cme, params)
 
@@ -73,3 +80,9 @@ def spectra(egams, cme, params):
     specs = {'total': total, 'mu mu': muons, 'e e': electrons}
 
     return specs
+
+
+def gamma_ray_lines(cme, params):
+    bf = branching_fractions(cme, params)["g g"]
+
+    return {"g g": {"energy": cme / 2.0, "bf": bf}}
