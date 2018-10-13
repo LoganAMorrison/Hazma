20b43a8f98ea7f44abf3a3a8475c3798be48c079
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2018-10-12 22:09:30 -0700

    Changing version number

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ extensions += [Extension(vm_pack + ".vector_mediator_decay_spectrum",
                                   "vector_mediator_decay_spectrum.pyx"])]
 
 setup(name='hazma',
-      version='0.43',
+      version='0.44',
       author='Logan Morrison and Adam Coogan',
       author_email='loanmorr@ucsc.edu',
       maintainer='Logan Morrison',
