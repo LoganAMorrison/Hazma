e9db0437fa325115b8d674824a52ec386d6ae5ad
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2018-10-13 21:22:03 -0700

    Trying to fix venv c extension installation

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -3,7 +3,6 @@ $PYTHON -m pip --version
 $PYTHON -m pip install -q --user --ignore-installed --upgrade virtualenv
 $PYTHON -m virtualenv -p $PYTHON venv
 . venv/bin/activate
-python -m pip install -r requirements.txt
-python -m setup.py install
-python -m pip freeze
+pip install -r requirements.txt
+pip install --install-option="--prefix"
 python --version
