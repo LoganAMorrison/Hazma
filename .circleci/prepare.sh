05fa88412f4adb4f13603c52e230082d1fe61675
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2018-10-13 20:32:30 -0700

    Fixing requirements.txt type and py.test to venv/bin/py.test

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -2,7 +2,7 @@ $PYTHON --version
 $PYTHON -m pip --version
 $PYTHON -m pip install -q --user --ignore-installed --upgrade virtualenv
 $PYTHON -m virtualenv -p $PYTHON venv
-venv/bin/python -m pip install -r requirements-dev.txt
+venv/bin/python -m pip install -r requirements.txt
 venv/bin/python -m pip install cibuildwheel
 venv/bin/python -m pip freeze
 venv/bin/python --version
