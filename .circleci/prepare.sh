952ccf632cfb48e505c892b8b12bd3b769974cae
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2018-10-13 20:36:50 -0700

    Added hazma to installs..

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -3,6 +3,6 @@ $PYTHON -m pip --version
 $PYTHON -m pip install -q --user --ignore-installed --upgrade virtualenv
 $PYTHON -m virtualenv -p $PYTHON venv
 venv/bin/python -m pip install -r requirements.txt
-venv/bin/python -m pip install cibuildwheel
+venv/bin/python -m pip install .
 venv/bin/python -m pip freeze
 venv/bin/python --version
