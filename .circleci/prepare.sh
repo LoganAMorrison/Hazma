00e38a9d39886bdeecbae21db3acb4ce163c7a9f
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2018-10-13 21:14:55 -0700

    sourcing venv

--- .circleci/prepare.sh
+++ .circleci/prepare.sh
@@ -2,7 +2,8 @@ $PYTHON --version
 $PYTHON -m pip --version
 $PYTHON -m pip install -q --user --ignore-installed --upgrade virtualenv
 $PYTHON -m virtualenv -p $PYTHON venv
-venv/bin/python -m pip install -r requirements.txt
-venv/bin/python -m pip install .
-venv/bin/python -m pip freeze
-venv/bin/python --version
+. venv/bin/activate
+python -m pip install -r requirements.txt
+python -m setup.py install
+python -m pip freeze
+python --version
