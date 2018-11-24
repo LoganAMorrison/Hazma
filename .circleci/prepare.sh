b7d9d0479b320dacc843cca5f14d261b829543f9
Logan Morrison <loganmorrison@Logans-MacBook-Pro.local>
2018-11-23 20:02:11 -0800

    Reverting circleci back to old yml and removing deploy

--- .circleci/prepare.sh
+++
@@ -1,8 +0,0 @@
-$PYTHON --version
-$PYTHON -m pip --version
-$PYTHON -m pip install -q --user --ignore-installed --upgrade virtualenv
-$PYTHON -m virtualenv -p $PYTHON venv
-venv/bin/python -m pip install -r requirements.txt
-venv/bin/python -m pip install cibuildwheel
-venv/bin/python -m pip freeze
-venv/bin/python --version
--- .circleci/prepare_cibw.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pip install --upgrade pip
-pip install numpy
-pip install scipy
-pip install cython
-pip instal --upgrade setuptools
-pip install scikit-image
-pip install .
