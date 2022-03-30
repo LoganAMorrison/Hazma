15ec60dd3532f3ed44f0675fd4ae0f3ba7963a83
Logan A. Morrison <loganmorrison99@gmail.com>
2022-03-30 10:24:34 -0700

    [feat] Adding the `PhaseSpace` type.
    `PhaseSpace` is a pure numpy version of the rambo algorithm.
    It typically seems to outperform the cython version
    (likely due to my inefficient implementation.)

--- hazma/rambo.py
+++ hazma/rambo.py
@@ -13,8 +13,10 @@ TODO: Code up specific functions for cross-section
 from typing import Union, List, Optional, Callable, Tuple
 import warnings
 import multiprocessing as mp
+import math
 
 import numpy as np
+import numpy.typing as npt
 
 from hazma._phase_space import generator
 from hazma._phase_space import histogram
@@ -481,3 +483,378 @@ def compute_decay_width(
     error = std / (2.0 * cme)
 
     return width, error
+
+
+class PhaseSpace:
+    def __init__(
+        self,
+        cme: float,
+        masses: npt.NDArray,
+        msqrd: Optional[SquaredMatrixElement] = None,
+    ) -> None:
+        """
+        Parameters
+        ----------
+        cme: float
+            Center-of-mass energy of the proccess.
+        masses: array-like
+            Array of the final state particle masses.
+        msqrd: Callable[[ndarray], float], optional
+            Squared matrix element of the proccess.
+        """
+        n = len(masses)
+
+        self.__cme = cme
+        self.__masses = np.array(masses).reshape((n, 1))
+        self.__msqrd: Optional[SquaredMatrixElement] = msqrd
+
+        self.__n = n
+        self.__xi0 = np.sqrt(1.0 - (sum(masses) / cme) ** 2)
+        self.__base_wgt = self._compute_base_weight()
+
+        self.__rng = np.random.default_rng()
+
+        self.__eps = 2 ** (-52)
+
+    @property
+    def cme(self) -> float:
+        """
+        Center-of-mass energy of the proccess.
+        """
+        return self.__cme
+
+    @cme.setter
+    def cme(self, val) -> None:
+        self.__cme = val
+        self.__base_wgt = self._compute_base_weight()
+
+    @property
+    def masses(self) -> np.ndarray:
+        """
+        Masses of the final state particles.
+        """
+        return self.__masses
+
+    @masses.setter
+    def masses(self, masses: npt.NDArray) -> None:
+        self.__n = len(masses)
+        self.__masses = np.array(masses).reshape((self.__n, 1))
+        self.__base_wgt = self._compute_base_weight()
+
+    @property
+    def msqrd(self) -> Optional[SquaredMatrixElement]:
+        """
+        Squared matrix element of the proccess.
+        """
+        return self.__msqrd
+
+    def _compute_base_weight(self) -> float:
+        """
+        Compute the starting weight for each phase space point.
+        """
+        cme = self.__cme
+        n = self.__n
+
+        fact_nm2 = math.factorial(n - 2)
+        fact = 1.0 / ((n - 1) * fact_nm2 ** 2)
+
+        return (
+            fact
+            * (0.5 * np.pi) ** (n - 1)
+            * cme ** (2 * n - 4)
+            * (0.5 / np.pi) ** (3 * n - 4)
+        )
+
+    def _initialize_momenta(self, batch_size: int, dtype=np.float64) -> np.ndarray:
+        """
+        Initialize isotropic and massless momenta with energyies
+        distributed according to E exp(-E).
+
+        Parameters
+        ----------
+        batch_size: int
+            Number of phase space points to generate.
+
+        Returns
+        -------
+        momenta: ndarray
+            Array containing the initialized four momenta with shape
+            (4, # final state particles, batch_size). The 1st dimension
+            contains the energy, x-, y- and z-components of the 3-momenta. The
+            2nd dimension runs over all final state particles. The 3rd
+            dimension runs over all events.
+        """
+        n = self.__n
+
+        rho1 = self.__rng.random(size=(n, batch_size), dtype=dtype)
+        rho2 = self.__rng.random(size=(n, batch_size), dtype=dtype)
+        rho3 = self.__rng.random(size=(n, batch_size), dtype=dtype)
+        rho4 = self.__rng.random(size=(n, batch_size), dtype=dtype)
+
+        ctheta = 2 * rho1 - 1.0
+        stheta = np.sqrt(1.0 - ctheta ** 2)
+        phi = 2.0 * np.pi * rho2
+        e = -np.log(rho3 * rho4)
+
+        return np.array(
+            [e, e * stheta * np.cos(phi), e * stheta * np.sin(phi), e * ctheta]
+        )
+
+    def _boost(self, ps: np.ndarray) -> np.ndarray:
+        """
+        Boost momenta into the center-of-mass frame. Input should be the
+        result of `_initialize_momenta`.
+
+        Parameters
+        ----------
+        ps: ndarray
+            Momenta generated from `_initialize_momenta`.
+
+        Returns
+        -------
+        momenta: ndarray
+            Momenta in the center-of-mass frame. The shape is idential to
+            input shape.
+        """
+        sum_ps = np.sum(ps, axis=1)
+        inv_mass = np.sqrt(
+            sum_ps[0] ** 2 - sum_ps[1] ** 2 - sum_ps[2] ** 2 - sum_ps[3] ** 2
+        )
+        inv_mass = 1.0 / inv_mass
+
+        bx = -inv_mass * sum_ps[1]
+        by = -inv_mass * sum_ps[2]
+        bz = -inv_mass * sum_ps[3]
+
+        x = self.__cme * inv_mass
+        g = sum_ps[0] * inv_mass
+        a = 1.0 / (1.0 + g)
+
+        bdotp = bx * ps[1] + by * ps[2] + bz * ps[3]
+        fact = a * bdotp + ps[0]
+
+        ps[0] = x * (g * ps[0] + bdotp)
+        ps[1] = x * (fact * bx + ps[1])
+        ps[2] = x * (fact * by + ps[2])
+        ps[3] = x * (fact * bz + ps[3])
+
+        return ps
+
+    def _compute_scale_factor(self, ps: np.ndarray, iterations=5) -> np.ndarray:
+        """
+        Compute the scale factor needed to correct the masses of the final
+        state particle momenta.
+
+        Parameters
+        ----------
+        ps: ndarray
+            Momenta returned from `_boost`.
+
+        Returns
+        -------
+        xi: ndarray
+            Scale factors for each phase-space point. The shape is
+            (batch_size,) where `batch_size` is the size of the last
+            dimension of `ps`.
+        """
+        cme = self.__cme
+        xi0 = self.__xi0
+
+        shape = ps.shape[1:]
+        e = ps[0]
+        xi = xi0 * np.ones((shape[-1],))
+        it = 0
+        last = False
+
+        while not last:
+            deltaf = np.hypot(e * xi, self.__masses)
+            f = np.sum(deltaf, axis=0) - cme
+            df = np.sum(xi * e ** 2 / deltaf, axis=0)
+            xi = xi - f / df
+
+            # Newton iterations have quadratic convergence. If we are below sqrt(eps),
+            # do only one more iteration
+            if np.max(f) < np.sqrt(self.__eps) or it == iterations - 1:
+                last = True
+
+            it = it + 1
+
+        return xi
+
+    def _correct_masses(self, ps: np.ndarray) -> np.ndarray:
+        """
+        Correct the masses of the final state particles.
+
+        Parameters
+        ----------
+        ps: ndarray
+            Momenta returned from `_boost`.
+
+        Returns
+        -------
+        ps: ndarray
+            Momenta with corrected masses. The shape is identical to input.
+        """
+        xi = self._compute_scale_factor(ps)
+        ps[0] = np.hypot(xi * ps[0], self.__masses)
+        ps[1:] = xi * ps[1:]
+        return ps
+
+    def _compute_weights(self, ps):
+        """
+        Compute the weights of phase space points.
+
+        Parameters
+        ----------
+        ps: ndarray
+            Momenta returned from `_correct_masses`.
+
+        Returns
+        -------
+        weights: ndarray
+            Weights of the phase space points. The shape is (batch_size, )
+            where `batch_size` is the size of the last dimension of `ps`.
+        """
+        cme = self.__cme
+        n = self.__n
+
+        modsqr = np.sum(ps[1:] ** 2, axis=0)
+        mod = np.sqrt(modsqr)
+
+        t1 = np.sum(mod / cme, axis=0) ** (2 * n - 3)
+        t2 = np.sum(modsqr / ps[0], axis=0)
+        t3 = np.prod(mod / ps[0], axis=0)
+
+        return t1 / t2 * t3 * self.__cme * self.__base_wgt
+
+    def generate(self, n, seed: Optional[int] = None, dtype=np.float64):
+        """
+        Generate four-momenta and weights distributed according to
+        Lorentz-invariant phase space.
+
+        Parameters
+        ----------
+        n: int
+            Number of phase space points to generate.
+        seed: int, optional
+            Seed used for numpy random number generator.
+        dtype: DTypeLike, optional
+            Type used for generation of momenta and weights.
+
+        Returns
+        -------
+        momenta: ndarray
+            Array containing the initialized four momenta with shape
+            (4, # final state particles, n). The 1st dimension
+            contains the energy, x-, y- and z-components of the 3-momenta. The
+            2nd dimension runs over all final state particles. The 3rd
+            dimension runs over all events.
+        weights: ndarray
+            Array containing weights of the phase space points. Shape is given by
+            (n,).
+        """
+        if dtype == np.float64:
+            self.__eps = 2 ** (-52)
+        elif dtype == np.float32:
+            self.__eps = 2 ** (-23)
+
+        self.__rng = np.random.default_rng(seed)
+
+        ps = self._initialize_momenta(n, dtype=dtype)
+        ps = self._boost(ps)
+        ps = self._correct_masses(ps)
+        ws = self._compute_weights(ps)
+
+        if self.__msqrd is not None:
+            ws *= self.__msqrd(ps)
+
+        return ps, ws
+
+    def integrate(
+        self, n: int, seed: Optional[int] = None, dtype=np.float64
+    ) -> Tuple[float, float]:
+        """
+        Integrate over phase space.
+
+        Parameters
+        ----------
+        n: int
+            Number of phase space points used in integration.
+        seed: int, optional
+            Seed used for numpy random number generator.
+        dtype: DTypeLike, optional
+            Type used for generation of momenta and weights.
+
+        Returns
+        -------
+        integral: float
+            Value of the phase space integration.
+        error: float
+            Estimation of the error.
+        """
+        _, weights = self.generate(n, seed=seed, dtype=dtype)
+
+        integral = np.nanmean(weights, dtype=np.float64)
+        error = np.nanstd(weights, dtype=np.float64, ddof=1) / np.sqrt(n)
+
+        return integral, error
+
+    def decay_width(self, n, seed: Optional[int] = None, dtype=np.float64):
+        """
+        Compute the decay width.
+
+        Parameters
+        ----------
+        n: int
+            Number of phase space points used in integration.
+        seed: int, optional
+            Seed used for numpy random number generator.
+        dtype: DTypeLike, optional
+            Type used for generation of momenta and weights.
+
+        Returns
+        -------
+        width: float
+            Estimated value of the decay width.
+        error: float
+            Estimation of the error.
+        """
+        integral, std = self.integrate(n, seed=seed, dtype=dtype)
+
+        pre = 0.5 / self.__cme
+        width = pre * integral
+        error = pre * std
+
+        return width, error
+
+    def cross_section(self, m1, m2, n, seed: Optional[int] = None, dtype=np.float64):
+        """
+        Compute the cross section given input masses.
+
+        Parameters
+        ----------
+        m1: float
+            Mass of the 1st incoming particle.
+        m2: float
+            Mass of the 2nd incoming particle.
+        n: int
+            Number of phase space points used in integration.
+        seed: int, optional
+            Seed used for numpy random number generator.
+        dtype: DTypeLike, optional
+            Type used for generation of momenta and weights.
+
+        Returns
+        -------
+        cross_section: float
+            Estimated value of the cross-section.
+        error: float
+            Estimation of the error.
+        """
+        integral, std = self.integrate(n, seed=seed, dtype=dtype)
+
+        pre = cross_section_prefactor(m1, m2, self.__cme)
+        cross_section = pre * integral
+        error = pre * std
+
+        return cross_section, error
