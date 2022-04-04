4c3928b0716a48b7fc48449e8945e1ea93a5e83c
Logan A. Morrison <loganmorrison99@gmail.com>
2022-04-04 15:20:36 -0700

    Updating gev-VM tests

--- test/vector_mediator/herwig4dm/FPhiPi.py
+++ test/vector_mediator/herwig4dm/FPhiPi.py
@@ -8,15 +8,17 @@ from . import alpha
 # own parametrization, see arXiv:1911.11147
 
 # amplitudes etc
-amp = [0.045, 0.0315, 0.0]
-phase = [180.0, 0.0, 180.0]
+# amp = [0.045, 0.0315, 0.0]
+# phase = [180.0, 0.0, 180.0]
+amp = [0.177522453644825, 0.023840592398187477, 0.0]
+phase = [0.0, 123.82008351626034, 0.0]
 br4pi = [0.0, 0.33, 0.0]
 # rho masses and widths
 rhoMasses = [0.77526, 1.593, 1.909]
 rhoWidths = [0.1491, 0.203, 0.048]
 mPhi = 1.019461
-mpi = 0.1349766
-wgts = [
+mpi = 134.9768e-3
+wgts0 = [
     amp[0] * cmath.exp(complex(0.0, phase[0] / 180.0 * math.pi)),
     amp[1] * cmath.exp(complex(0.0, phase[1] / 180.0 * math.pi)),
     amp[2] * cmath.exp(complex(0.0, phase[2] / 180.0 * math.pi)),
@@ -47,11 +49,11 @@ def resetParameters(gDM, mDM, mMed, wMed, cMedu, cMedd, cMeds):
     cI1_ = cMedu - cMedd
     cI0_ = 3 * (cMedu + cMedd)
     cS_ = -3 * cMeds
-    wgts = [weight * cI1_ for weight in wgts]
+    wgts = [weight * cI1_ for weight in wgts0]
 
 
 def GammaDM(M):
-    sHat = M ** 2
+    sHat = M**2
     if sHat < (mpi + mPhi) ** 2 or cI1_ == 0:
         return 0
     ecms = math.sqrt(sHat)
@@ -64,11 +66,11 @@ def GammaDM(M):
             + br4pi[ix]
             * mR2
             / sHat
-            * ((sHat - 16.0 * mpi ** 2) / (mR2 - 16.0 * mpi ** 2) ** 1.5)
+            * ((sHat - 16.0 * mpi**2) / (mR2 - 16.0 * mpi**2)) ** 1.5
         )
         form += wgts[ix] * mR2 / (mR2 - sHat - complex(0.0, 1.0) * ecms * wid)
     pcm = 0.5 * ecms * Resonance.beta(sHat, mPhi, mpi)
-    output = 1 / 12.0 / math.pi * pcm ** 3 * abs(form) ** 2
+    output = 1 / 12.0 / math.pi * pcm**3 * abs(form) ** 2
     return output
 
 
@@ -85,14 +87,14 @@ def sigmaSMPhiPi(sHat):
             + br4pi[ix]
             * mR2
             / sHat
-            * ((sHat - 16.0 * mpi ** 2) / (mR2 - 16.0 * mpi ** 2) ** 1.5)
+            * ((sHat - 16.0 * mpi**2) / (mR2 - 16.0 * mpi**2) ** 1.5)
         )
         pre += wgts[ix] * mR2 / (mR2 - sHat - complex(0.0, 1.0) * ecms * wid)
     pcm = 0.5 * ecms * Resonance.beta(sHat, mPhi, mpi)
     # phase-space, |me|^2 factors
-    output = 2.0 * pcm ** 3 / 8.0 / math.pi / ecms * abs(pre) ** 2 / 3
+    output = 2.0 * pcm**3 / 8.0 / math.pi / ecms * abs(pre) ** 2 / 3
     # initial-state factors
-    output *= 16.0 * math.pi ** 2 / sHat * alpha.alphaEM(sHat) ** 2
+    output *= 16.0 * math.pi**2 / sHat * alpha.alphaEM(sHat) ** 2
     return output * Resonance.gev2nb
 
 
@@ -110,20 +112,20 @@ def sigmaDMPhiPi(sHat):
             + br4pi[ix]
             * mR2
             / sHat
-            * ((sHat - 16.0 * mpi ** 2) / (mR2 - 16.0 * mpi ** 2) ** 1.5)
+            * ((sHat - 16.0 * mpi**2) / (mR2 - 16.0 * mpi**2)) ** 1.5
         )
         form += wgts[ix] * mR2 / (mR2 - sHat - complex(0.0, 1.0) * ecms * wid)
     pcm = 0.5 * ecms * Resonance.beta(sHat, mPhi, mpi)
     cDM = gDM_
-    DMmed = cDM / (sHat - mMed_ ** 2 + complex(0.0, 1.0) * mMed_ * wMed_)
+    DMmed = cDM / (sHat - mMed_**2 + complex(0.0, 1.0) * mMed_ * wMed_)
     DMmed2 = abs(DMmed) ** 2
     return (
         DMmed2
         / 12.0
         / math.pi
         * ecms
-        * (1 + 2 * mDM_ ** 2 / sHat)
-        * pcm ** 3
+        * (1 + 2 * mDM_**2 / sHat)
+        * pcm**3
         * abs(form) ** 2
         * Resonance.gev2nb
     )
--- test/vector_mediator/test_form_factors.py
+++ test/vector_mediator/test_form_factors.py
@@ -2,21 +2,25 @@
 Tests for the vector form factors.
 """
 # from typing import
-from typing import NamedTuple, Tuple
+from typing import NamedTuple, Tuple, Union, List
 import unittest
 
 import pytest
 from pytest import approx
 import numpy as np
 
-from hazma.vector_mediator import VectorMediatorGeV
+from hazma.vector_mediator import VectorMediatorGeV, KineticMixingGeV, BLGeV
+from hazma import parameters
 from hazma.parameters import Qd, Qe, Qu
 from hazma.parameters import qe
 from hazma.vector_mediator.form_factors import utils as ff_utils
 
 
+VectorModel = Union[VectorMediatorGeV, KineticMixingGeV, BLGeV]
+
+
 class HazmaFunction(NamedTuple):
-    model: VectorMediatorGeV
+    model: VectorModel
     function: str
     extra_args: Tuple
 
@@ -36,8 +40,8 @@ class ModuleFunction(NamedTuple):
 
 
 @pytest.fixture
-def model1() -> VectorMediatorGeV:
-    return VectorMediatorGeV(
+def models() -> List[VectorModel]:
+    model1 = VectorMediatorGeV(
         mx=5e3,
         mv=2e3,
         gvxx=1.0,
@@ -46,46 +50,51 @@ def model1() -> VectorMediatorGeV:
         gvss=-1.0,
         gvee=0.0,
         gvmumu=0.0,
+        gvveve=0.0,
+        gvvmvm=0.0,
+        gvvtvt=0.0,
     )
 
-
-@pytest.fixture
-def model2() -> VectorMediatorGeV:
-    return VectorMediatorGeV(
+    model2 = VectorMediatorGeV(
         mx=5e3,
-        mv=4e3,
+        mv=1.6e3,
         gvxx=1.0,
         gvuu=1.3,
         gvdd=1.2,
         gvss=1.1,
         gvee=0.0,
         gvmumu=0.0,
+        gvveve=0.0,
+        gvvmvm=0.0,
+        gvvtvt=0.0,
     )
 
-
-@pytest.fixture
-def kinetic_mixing_model() -> VectorMediatorGeV:
     eps = 1.0
-    return VectorMediatorGeV(
-        mx=5e3,
-        mv=2e3,
-        gvxx=1.0,
-        gvuu=Qu * eps * qe,
-        gvdd=Qd * eps * qe,
-        gvss=Qd * eps * qe,
-        gvee=Qe * eps * qe,
-        gvmumu=Qe * eps * qe,
-    )
+    km = KineticMixingGeV(mx=5e3, mv=2e3, gvxx=1.0, eps=eps)
+
+    bl = BLGeV(mx=5e3, mv=2e3, gvxx=1.0)
+
+    return [model1, model2, km, bl]
 
 
-def synchronize_herwig(model: VectorMediatorGeV, module) -> None:
+def error_msg(hazma, herwig, model):
+    return f"model:{model}, hazma, herwig = {hazma:.3e}, {herwig:.3e}"
+
+
+def synchronize_herwig(model: VectorModel, module) -> None:
     module.resetParameters(
-        model.gvxx, model.mx, model.mv, np.nan, model.gvuu, model.gvdd, model.gvss
+        model.gvxx,
+        model.mx * 1e-3,
+        model.mv * 1e-3,
+        np.nan,
+        model.gvuu,
+        model.gvdd,
+        model.gvss,
     )
 
 
 def compare_widths(
-    model: VectorMediatorGeV,
+    model: VectorModel,
     func: str,
     herwig_module,
     reltol=1e-4,
@@ -100,223 +109,251 @@ def compare_widths(
 
 
 def compare_form_factors(
-    hazma: HazmaFunction, herwig: ModuleFunction, herwig_units: int = -1
+    hazma: HazmaFunction, herwig: ModuleFunction, herwig_units: int = -1, reltol=1e-4
 ):
 
     synchronize_herwig(hazma.model, herwig.module)
     mvmev = hazma.model.mv
     mvgev = mvmev * 1e-3
     hazma_val = hazma(mvmev)
-    herwig_val = herwig(mvgev ** 2) / (mvgev ** herwig_units)
+    herwig_val = herwig(mvgev**2) / (mvgev**herwig_units)
 
-    assert np.real(hazma_val) == approx(np.real(herwig_val), rel=1e-4, abs=0.0)
-    assert np.imag(hazma_val) == approx(np.imag(herwig_val), rel=1e-4, abs=0.0)
+    assert np.real(hazma_val) == approx(np.real(herwig_val), rel=reltol, abs=0.0)
+    assert np.imag(hazma_val) == approx(np.imag(herwig_val), rel=reltol, abs=0.0)
 
 
-def test_form_factor_pipi(model1: VectorMediatorGeV):
+def test_form_factor_pi_pi(models: List[VectorModel]):
     """
     Test that the `hazma` and `herwig4DM` implementations of the V-pi-pi
     form factor agree for the input model.
     """
     from herwig4dm import F2pi
 
-    hazma = HazmaFunction(model1, "form_factor_pipi", (1,))
-    herwig = ModuleFunction(F2pi, "Fpi", (1,))
+    for model in models:
+        hazma = HazmaFunction(model, "form_factor_pi_pi", (1,))
+        herwig = ModuleFunction(F2pi, "Fpi", (1,))
+        compare_form_factors(hazma, herwig, 0)
 
-    compare_form_factors(hazma, herwig, 0)
 
-
-def test_form_factor_pi_gamma(
-    model1: VectorMediatorGeV,
-    model2: VectorMediatorGeV,
-    kinetic_mixing_model: VectorMediatorGeV,
-):
+def test_form_factor_pi_gamma(models: List[VectorModel]):
     """
     Test that the `hazma` and `herwig4DM` implementations of the V-pi-gamma
     form factor agree for the input model.
     """
     from herwig4dm import FPiGamma
 
-    hazma = HazmaFunction(model1, "_form_factor_pi_gamma", tuple())
-    herwig = ModuleFunction(FPiGamma, "FPiGamma", tuple())
-
-    for model in [model1, model2, kinetic_mixing_model]:
+    for model in models:
         hazma = HazmaFunction(model, "_form_factor_pi_gamma", tuple())
+        herwig = ModuleFunction(FPiGamma, "FPiGamma", tuple())
         compare_form_factors(hazma, herwig)
 
 
-def test_form_factor_pi_gamma2(model2: VectorMediatorGeV):
-    """
-    Test that the `hazma` and `herwig4DM` implementations of the V-pi-gamma
-    form factor agree for the input model.
-    """
-    from herwig4dm import FPiGamma
-
-    hazma = HazmaFunction(model2, "_form_factor_pi_gamma", tuple())
-    herwig = ModuleFunction(FPiGamma, "FPiGamma", tuple())
-
-    compare_form_factors(hazma, herwig)
-
-
-def test_form_factor_eta_gamma(model1: VectorMediatorGeV):
+def test_form_factor_eta_gamma(models: List[VectorModel]):
     """
     Test that the `hazma` and `herwig4DM` implementations of the V-eta-gamma
     form factor agree for the input model.
     """
     from herwig4dm import FEtaGamma
 
-    hazma = HazmaFunction(model1, "_form_factor_eta_gamma", tuple())
-    herwig = ModuleFunction(FEtaGamma, "FEtaGamma", tuple())
+    for model in models:
+        hazma = HazmaFunction(model, "_form_factor_eta_gamma", tuple())
+        herwig = ModuleFunction(FEtaGamma, "FEtaGamma", tuple())
 
-    compare_form_factors(hazma, herwig)
+        compare_form_factors(hazma, herwig)
 
 
-def test_form_factor_k0k0(model1: VectorMediatorGeV):
+def test_form_factor_k0_k0(models: List[VectorModel]):
     """
     Test that the `hazma` and `herwig4DM` implementations of the K0 K0
     form factor agree for the input model.
     """
     from herwig4dm import FK
 
-    hazma = HazmaFunction(model1, "form_factor_kk", (0,))
-    herwig = ModuleFunction(FK, "Fkaon", (0,))
+    for model in models:
+        hazma = HazmaFunction(model, "form_factor_k_k", (0,))
+        herwig = ModuleFunction(FK, "Fkaon", (0,))
+        compare_form_factors(hazma, herwig, 0, reltol=5e-1)
 
-    compare_form_factors(hazma, herwig, 0)
 
-
-def test_form_factor_kk(model1: VectorMediatorGeV):
+def test_form_factor_kk(models: List[VectorModel]):
     """
     Test that the `hazma` and `herwig4DM` implementations of the K K
     form factor agree for the input model.
     """
     from herwig4dm import FK
 
-    hazma = HazmaFunction(model1, "form_factor_kk", (1,))
-    herwig = ModuleFunction(FK, "Fkaon", (1,))
+    for model in models:
+        hazma = HazmaFunction(model, "form_factor_k_k", (1,))
+        herwig = ModuleFunction(FK, "Fkaon", (1,))
+        compare_form_factors(hazma, herwig, 0)
 
-    compare_form_factors(hazma, herwig, 0)
 
-
-def test_form_factor_omega_pi(model1: VectorMediatorGeV):
+def test_form_factor_pi_omega(models: List[VectorModel]):
     """
     Test that the `hazma` and `herwig4DM` implementations of the V-omega-pi
     form factor agree for the input model.
     """
     from herwig4dm import FOmegaPion
 
-    hazma = HazmaFunction(model1, "_form_factor_omega_pi", tuple())
-    herwig = ModuleFunction(FOmegaPion, "FOmPiGamma", tuple())
+    for model in models:
+        hazma = HazmaFunction(model, "_form_factor_pi_omega", tuple())
+        herwig = ModuleFunction(FOmegaPion, "FOmPiGamma", tuple())
+        compare_form_factors(hazma, herwig)
 
-    compare_form_factors(hazma, herwig)
 
-
-def test_width_omega_pi(model1: VectorMediatorGeV):
+def test_width_pi0_omega(models: List[VectorModel]):
     """
     Test that the `hazma` and `herwig4DM` implementations of the V-omega-pi
     widths agree for the input model.
     """
-    from herwig4dm import FOmegaPion
+    from herwig4dm import FOmegaPion as herwig_module
 
-    compare_widths(model1, "width_v_to_omega_pi", FOmegaPion)
+    model_names = ["model1", "model2", "km", "bl"]
+
+    for i, model in enumerate(models):
+        mvgev = model.mv * 1e-3
+        synchronize_herwig(model, herwig_module)
+        hazma = model.width_v_to_pi0_pi0_gamma() / model.mv / 8.34e-2
+        herwig = herwig_module.GammaDM(mvgev) / mvgev
+        assert hazma == approx(herwig, rel=1e-4, abs=0.0), error_msg(
+            hazma, herwig, model_names[i]
+        )
 
 
-def test_width_phi_pi(model1: VectorMediatorGeV):
+def test_width_phi_pi(models: List[VectorModel]):
     """
     Test that the `hazma` and `herwig4DM` implementations of the V-phi-pi
     widths agree for the input model.
     """
-    from herwig4dm import FPhiPi
+    from herwig4dm import FPhiPi as herwig_module
 
-    compare_widths(model1, "width_v_to_phi_pi", FPhiPi)
+    model_names = ["model1", "model2", "km", "bl"]
+
+    for i, model in enumerate(models):
+        # compare_widths(model, "width_v_to_pi0_phi", FPhiPi)
+        mvgev = model.mv * 1e-3
+        synchronize_herwig(model, herwig_module)
+        hazma = model.width_v_to_pi0_phi() / model.mv
+        herwig = herwig_module.GammaDM(mvgev) / mvgev
+        assert hazma == approx(herwig, rel=1e-4, abs=0.0), error_msg(
+            hazma, herwig, model_names[i]
+        )
 
 
-def test_width_eta_phi(model1: VectorMediatorGeV):
+def test_width_eta_phi(models: List[VectorModel]):
     """
     Test that the `hazma` and `herwig4DM` implementations of the V-eta-phi
     widths agree for the input model.
     """
     from herwig4dm import FEtaPhi
 
-    compare_widths(model1, "width_v_to_eta_phi", FEtaPhi)
+    for model in models:
+        compare_widths(model, "width_v_to_eta_phi", FEtaPhi)
 
 
-def test_width_eta_omega(model1: VectorMediatorGeV):
+def test_width_eta_omega(models: List[VectorModel]):
     """
     Test that the `hazma` and `herwig4DM` implementations of the V-eta-omega
     widths agree for the input model.
     """
     from herwig4dm import FEtaOmega
 
-    compare_widths(model1, "width_v_to_eta_omega", FEtaOmega)
+    for model in models:
+        compare_widths(model, "width_v_to_eta_omega", FEtaOmega)
 
 
-def test_width_pi_pi_pi0(model1: VectorMediatorGeV):
+def test_width_pi_pi_pi0(models: List[VectorModel]):
     from herwig4dm import F3pi
 
-    compare_widths(model1, "width_v_to_pi_pi_pi0", F3pi, reltol=1e-2)
+    for model in models:
+        compare_widths(model, "width_v_to_pi_pi_pi0", F3pi, reltol=1e-1)
 
 
-def test_width_pi_pi_eta(model1: VectorMediatorGeV):
+def test_width_pi_pi_eta(models: List[VectorModel]):
     from herwig4dm import FEtaPiPi
 
-    compare_widths(model1, "width_v_to_pi_pi_eta", FEtaPiPi)
+    for model in models:
+        compare_widths(model, "width_v_to_pi_pi_eta", FEtaPiPi)
 
 
-def test_width_pi_pi_etap(model1: VectorMediatorGeV):
+def test_width_pi_pi_etap(models: List[VectorModel]):
     from herwig4dm import FEtaPrimePiPi
 
-    compare_widths(model1, "width_v_to_pi_pi_etap", FEtaPrimePiPi, reltol=1e-2)
+    for model in models:
+        compare_widths(model, "width_v_to_pi_pi_etap", FEtaPrimePiPi, reltol=1e-2)
 
 
-def test_width_pi_pi_omega(model1: VectorMediatorGeV):
+def test_width_pi_pi_omega(models: List[VectorModel]):
     from herwig4dm import FOmegaPiPi
 
-    compare_widths(model1, "width_v_to_pi_pi_omega", FOmegaPiPi)
+    for model in models:
+        compare_widths(model, "width_v_to_pi_pi_omega", FOmegaPiPi)
 
 
-def test_width_pi0_k0_k0(model1: VectorMediatorGeV):
+def test_width_pi0_k0_k0(models: List[VectorModel]):
     from herwig4dm import FKKpi
 
     # compare_widths(model1, "width_v_to_pi0_k0_k0", FKKpi, herwig_args=(0,))
 
-    mvgev = model1.mv * 1e-3
-    synchronize_herwig(model1, FKKpi)
-    hazma = model1.width_v_to_pi0_k0_k0() / model1.mv
-    herwig = FKKpi.GammaDM(mvgev, imode=0) / mvgev
-    assert hazma == approx(herwig, rel=1e-3, abs=1e-10)
+    for model in models:
+        mvgev = model.mv * 1e-3
+        synchronize_herwig(model, FKKpi)
+        hazma = model.width_v_to_pi0_k0_k0() / model.mv
+        herwig = FKKpi.GammaDM(mvgev, imode=0) / mvgev
+        assert hazma == approx(herwig, rel=1e-1, abs=1e-10)
 
 
-def test_width_v_to_pi0_k_k(model1: VectorMediatorGeV):
+def test_width_v_to_pi0_k_k(models: List[VectorModel]):
     from herwig4dm import FKKpi
 
     # compare_widths(model1, "width_v_to_pi0_k_k", FKKpi, herwig_args=(1,))
 
-    mvgev = model1.mv * 1e-3
-    synchronize_herwig(model1, FKKpi)
-    hazma = model1.width_v_to_pi0_k0_k0() / model1.mv
-    herwig = FKKpi.GammaDM(mvgev, imode=1) / mvgev
-    assert hazma == approx(herwig, rel=1e-3, abs=1e-10)
+    for model in models:
+        mvgev = model.mv * 1e-3
+        synchronize_herwig(model, FKKpi)
+        hazma = model.width_v_to_pi0_k_k() / model.mv
+        herwig = FKKpi.GammaDM(mvgev, imode=1) / mvgev
+        assert hazma == approx(herwig, rel=1e-1, abs=1e-10)
 
 
-def test_width_v_to_pi_k_k0(model1: VectorMediatorGeV):
+def test_width_v_to_pi_k_k0(models: List[VectorModel]):
     from herwig4dm import FKKpi
 
     # compare_widths(model1, "width_v_to_pi0_k_k", FKKpi, herwig_args=(1,))
 
-    mvgev = model1.mv * 1e-3
-    synchronize_herwig(model1, FKKpi)
-    hazma = model1.width_v_to_pi_k_k0() / model1.mv
-    # herwig4dm includes both charged modes so divide by 2
-    herwig = FKKpi.GammaDM(mvgev, imode=2) / mvgev / 2
-    assert hazma == approx(herwig, rel=1e-3, abs=1e-10)
+    for model in models:
+        mvgev = model.mv * 1e-3
+        synchronize_herwig(model, FKKpi)
+        hazma = model.width_v_to_pi_k_k0() / model.mv
+        # herwig4dm includes both charged modes so divide by 2
+        herwig = FKKpi.GammaDM(mvgev, imode=2) / mvgev / 2
+        assert hazma == approx(herwig, rel=1e-1, abs=1e-10)
 
 
-# def test_width_v_to_pi_k_k0(model1: VectorMediatorGeV):
-#     from herwig4dm import FKKpi
+def test_width_v_to_pi_pi_pi_pi(models: List[VectorModel]):
+    from herwig4dm import F4pi
 
-#     compare_widths(model1, "width_v_to_pi_k_k0", FKKpi, herwig_args=(2,))
+    for model in models:
+        F4pi.cI1_ = 1.0
+        synchronize_herwig(model, F4pi)
+        F4pi.readHadronic_Current()
+
+        mvmev = model.mv
+        mvgev = mvmev * 1e-3
+        hazma = model.width_v_to_pi_pi_pi_pi(npts=1 << 15) / mvmev
+        herwig = F4pi.GammaDM(mvgev, "charged") / mvgev
+        assert hazma == approx(herwig, rel=1e-1, abs=0.0)
 
 
-# class TestFormFactorUtils(unittest.TestCase):
-#     def test_integral_power_div_simple_monomial(self):
-#         ff_utils.integral_power_div_simple_monomial(1, 2, 0, 1)
+def test_width_v_to_pi_pi_pi0_pi0(models: List[VectorModel]):
+    from herwig4dm import F4pi
+
+    for model in models:
+        F4pi.cI1_ = 1.0
+        synchronize_herwig(model, F4pi)
+        F4pi.readHadronic_Current()
+        mvmev = model.mv
+        mvgev = mvmev * 1e-3
+        hazma = model.width_v_to_pi_pi_pi0_pi0(npts=1 << 15) / mvmev
+        herwig = F4pi.GammaDM(mvgev, "neutral") / mvgev
+        assert hazma == approx(herwig, rel=1e-1, abs=0.0)
