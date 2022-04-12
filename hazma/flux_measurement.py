f3b3315f64b3dfdf02d6948ad93cd1732639ce3e
Logan A. Morrison <loganmorrison99@gmail.com>
2022-04-12 10:13:12 -0700

    [chore] Making docstring look better in sphinx

--- hazma/flux_measurement.py
+++ hazma/flux_measurement.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class FluxMeasurement:
-    """
+    r"""
 
     Container for all information about a completed gamma ray analysis.
 
@@ -13,15 +13,16 @@ class FluxMeasurement:
     e_highs : np.array
         Upper edges of energy bins.
     fluxes : np.array
-        Flux measurements for each bin (MeV^-1 cm^-2 s^-1 sr^-1).
+        Flux measurements for each bin
+        (:math:`\mathrm{MeV}^{-1}\mathrm{cm}^{-2} \mathrm{s}^{-1} \mathrm{sr}^{-1}`).
     upper_errors : np.array
-        Size of upper error bars on flux measurements (MeV^-1 cm^-2 s^-1
-        sr^-1).
+        Size of upper error bars on flux measurements
+        (:math:`\mathrm{MeV}^{-1}\mathrm{cm}^{-2} \mathrm{s}^{-1} \mathrm{sr}^{-1}`).
     lower_errors : np.array
-        Size of lower error bars on flux measurements (MeV^-1 cm^-2 s^-1
-        sr^-1).
+        Size of lower error bars on flux measurements
+        (:math:`\mathrm{MeV}^{-1}\mathrm{cm}^{-2} \mathrm{s}^{-1} \mathrm{sr}^{-1}`).
     energy_res : callable
-        Function returning energy resolution (Delta E / E) as a function of
+        Function returning energy resolution (:math:`\Delta E / E`) as a function of
         photon energy.
     target : TargetParams
         Information about the target observed for this measurement.
@@ -49,9 +50,12 @@ class FluxMeasurement:
 
                 1. Lower bin edge (MeV)
                 2. Upper bin edge (MeV)
-                3. :math:`E^2 d^2 \Phi/dE d\Omega` (MeV cm^-2 s^-1 sr^-1)
-                4. Upper error bar (MeV cm^-2 s^-1 sr^-1)
-                5. Lower error bar (MeV cm^-2 s^-1 sr^-1)
+                3. :math:`E^2 d^2 \Phi/dE d\Omega`
+                   (:math:`\mathrm{MeV}^{-1}\mathrm{cm}^{-2}\mathrm{s}^{-1}\mathrm{sr}^{-1}`).
+                4. Upper error bar
+                   (:math:`\mathrm{MeV}^{-1}\mathrm{cm}^{-2}\mathrm{s}^{-1}\mathrm{sr}^{-1}`).
+                5. Lower error bar
+                   (:math:`\mathrm{MeV}^{-1}\mathrm{cm}^{-2}\mathrm{s}^{-1}\mathrm{sr}^{-1}`).
 
             Note that the error bar values are their y-coordinates, not their
             relative distances from the central flux.
@@ -70,11 +74,11 @@ class FluxMeasurement:
         self._e_bins = 0.5 * (self.e_lows + self.e_highs)
 
         # E^2 dN/dE -> dN/dE
-        self.fluxes /= self._e_bins ** power
+        self.fluxes /= self._e_bins**power
 
         # Compute upper and lower error bars
-        self.upper_errors = self.upper_errors / self._e_bins ** power - self.fluxes
-        self.lower_errors = self.fluxes - self.lower_errors / self._e_bins ** power
+        self.upper_errors = self.upper_errors / self._e_bins**power - self.fluxes
+        self.lower_errors = self.fluxes - self.lower_errors / self._e_bins**power
 
         # Load energy resolution
         self.energy_res = energy_res
