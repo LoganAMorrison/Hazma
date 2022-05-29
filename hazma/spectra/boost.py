71ad5eef4380ee3387544dafd6600abf88080fc7
Logan A. Morrison <loganmorrison99@gmail.com>
2022-05-28 20:56:16 -0700

    [feat] Adding a booster function
    Added a couple of functions to compute the boost integrals and to
    generate new functions capable of computing the boost spectrum

--- hazma/spectra/boost.py
+++ hazma/spectra/boost.py
@@ -1,4 +1,10 @@
+from typing import Callable, Optional, Tuple
+from inspect import signature
+import warnings
+
 import numpy as np
+from scipy import interpolate
+from scipy import integrate
 
 
 def boost_delta_function(e, e0: float, m: float, beta: float):
@@ -76,3 +82,256 @@ def double_boost_delta_function(e2, e0: float, m: float, beta1: float, beta2: fl
     res = res / (4.0 * gamma1 * gamma2 * beta1 * beta2 * e0)
 
     return res
+
+
+def dnde_boost_array(dnde, energies, beta: float, mass: float = 0.0):
+    """Boost a spectrum dN/dE given as a numeric array.
+
+    Parameters
+    ----------
+    dnde: array
+        Spectrum to boost.
+    energies: array
+        Energies corresponding to `dnde`.
+    beta: float
+        Boost velocity. If `beta` is outside [0,1), zeros are returned.
+    mass: float
+        Mass of the product of the spectrum (i.e. 0 for photon, electron-mass
+        for positron).
+
+    Notes
+    -----
+    The boosted spectrum is computed by creating a linear interpolating
+    function from the data and using it to compute the integral.
+
+    Returns
+    -------
+    dnde_boosted: array
+        The boosted spectrum.
+    """
+    if beta < np.finfo(float).eps:
+        return dnde
+
+    if beta < 0 or beta > 1.0:
+        return np.zeros_like(energies)
+
+    gamma = 1.0 / np.sqrt(1 - beta**2)
+
+    boosted = np.zeros_like(energies)
+    mask = energies < mass
+    es = energies[mask]
+    k = np.sqrt(es**2 - mass**2)
+    emax = gamma * (es + beta * k)
+    emin = gamma * (es - beta * k)
+
+    integrand = dnde[mask] / k
+    pre = 0.5 / (2 * beta * gamma)
+
+    spline = interpolate.UnivariateSpline(es, integrand, ext=1, k=1)
+    boosted[mask] = np.array([spline.integral(a, b) for a, b in zip(emin, emax)])
+
+    return pre * boosted
+
+
+def make_boost_function(fn: Callable):
+    """Create a function to compute boost spectrum.
+
+    Parameters
+    ----------
+    fn: Callable
+        Spectrum function to transform.
+
+    Notes
+    -----
+    The boosted spectrum is computed by creating a linear interpolating
+    function from the data and using it to compute the integral.
+
+    Returns
+    -------
+    boosted_fn: Callable
+        The boosted function has the signature
+
+            (energies, beta,mass=0,method="quad"),
+
+        where:
+            - `energies`: Array of energies where boosted spectrum should be evaluated,
+            - `beta`: Boost velocity. If outside [0, 1), zeros are returned,
+            - `mass`: Mass of the product,
+            - `method`: Method used to integrate. Can be 'quad' for adaptive
+              quadrature, 'trapz' for trapizoidal rule or 'simpz' for Simpson's
+              rule.
+            - `kwargs`: Keyword arguments to pass to underlying method. See
+              SciPy's quad available arguments. For `trapizoid` or `simpson`,
+              the number of points to sample from integrand can be specified
+              through `npts`.
+
+        That is, the returned function looks like:
+
+            ```python
+            def boosted(energies, beta: float, mass: float = 0.0):
+                ...
+            ```
+    """
+    methods = ["quad", "trapizoid", "simpson"]
+
+    def bounds_and_mask(energies, gamma: float, beta: float, mass: float):
+        mask = energies > mass
+        es = energies[mask]
+        k = np.sqrt(es**2 - mass**2)
+        emin = gamma * (es - beta * k)
+        emax = gamma * (es + beta * k)
+
+        return emin, emax, mask
+
+    def kinematic_early_return(energies, beta: float):
+        if beta < np.finfo(float).eps:
+            return fn(energies)
+
+        if beta < 0.0 or beta > 1.0:
+            warnings.warn(f"Unphysical beta = {beta}. Returning zero.")
+            return np.zeros_like(energies)
+
+        return None
+
+    def check_method(method: str):
+        if method not in methods:
+            raise ValueError(f"Invalid method {method}. Use of one {methods}.")
+
+    def get_quad_kwargs(**kwargs):
+        positional = ["func", "a", "b"]
+
+        def use(key):
+            return key not in positional and kwargs.get(key) is not None
+
+        return {
+            key: kwargs.get(key, val)
+            for key, val in signature(integrate.quad).parameters.items()
+            if use(key)
+        }
+
+    def fn_boosted(
+        energies,
+        beta: float,
+        mass: float = 0.0,
+        method: str = "quad",
+        args: Tuple = tuple(),
+        **kwargs,
+    ):
+        """Compute the boosted spectrum.
+
+        Parameters
+        ----------
+        energies: array
+            Array of energies where spectrum should be evaluated.
+        beta: float
+            Boost velocity. If beta < 0 or beta > 1, zeros are returned.
+        mass: float, optional
+            Mass of the product. Default is zero (i.e. for a photon.)
+        method: str, optional
+            Method to use to integrate. Can be 'quad', 'trapizoid' or
+            'simpson'. Default is 'quad'.
+        args: tuple, optional
+            Additional arguments to pass to function.
+
+        Returns
+        -------
+        boosted: array
+            The boosted spectrum evaluated at the input energies.
+
+        Other Parameters
+        ----------------
+        If `method` = 'trapizoid' or 'simpson', the following keyword arguments
+        are available:
+
+            npts: int, optional
+                Number of points to use in trapizoidal or simpson integration.
+                Default is 100.
+            args: tuple, optional
+                Additional arguments to pass to the wrapped spectrum function.
+
+        If method = 'quad', any keyword arguments compatible with quad can be
+        specified.
+
+        """
+        check_method(method)
+        early = kinematic_early_return(energies, beta)
+        if early is not None:
+            return early
+
+        gamma = 1.0 / np.sqrt(1.0 - beta**2)
+        boosted = np.zeros_like(energies)
+        emin, emax, mask = bounds_and_mask(
+            energies=energies, gamma=gamma, beta=beta, mass=mass
+        )
+
+        if args is not None:
+
+            def integrand(e):
+                return fn(e, *args) / np.sqrt(e**2 - mass**2)
+
+        else:
+
+            def integrand(e):
+                return fn(e) / np.sqrt(e**2 - mass**2)
+
+        bounds = zip(emin, emax)
+
+        if method == "quad":
+            quad_kwargs = get_quad_kwargs(**kwargs)
+            boosted[mask] = np.array(
+                [integrate.quad(integrand, a, b, **quad_kwargs)[0] for a, b in bounds]
+            )
+        else:
+            npts = kwargs.get("npts", 100)
+            es = np.array([np.linspace(a, b, npts) for a, b in bounds])
+            integrands = np.array([integrand(e) for e in es])
+
+            if method == "trapizoid":
+                integrator = integrate.trapezoid
+            else:
+                integrator = integrate.simpson
+
+            boosted[mask] = integrator(integrands, es)
+
+        return 0.5 * boosted / (beta * gamma)
+
+    return fn_boosted
+
+
+def dnde_boost(
+    fn: Callable,
+    energies,
+    beta: float,
+    mass: float = 0.0,
+    method: str = "quad",
+    args: Tuple = tuple(),
+    **kwargs,
+):
+    """Boost a spectrum dN/dE given as a function.
+
+    Parameters
+    ----------
+    energies: array
+        Array of energies where spectrum should be evaluated.
+    beta: float
+        Boost velocity. If beta < 0 or beta > 1, zeros are returned.
+    mass: float, optional
+        Mass of the product. Default is zero (i.e. for a photon.)
+    method: str, optional
+        Method to use to integrate. Can be 'quad', 'trapizoid' or
+        'simpson'. Default is 'quad'.
+    args: tuple, optional
+        Additional arguments to pass to function.
+
+    Notes
+    -----
+    The boosted spectrum is computed by creating a linear interpolating
+    function from the data and using it to compute the integral.
+
+    Returns
+    -------
+    dnde_boosted: array
+        The boosted spectrum.
+    """
+    boosted = make_boost_function(fn)
+    return boosted(energies, beta, mass=mass, method=method, args=args, **kwargs)
