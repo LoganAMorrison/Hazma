d54b45b3fe9c22d3e44fbf4b4807e1b5e362d5c8
Logan A. Morrison <loganmorrison99@gmail.com>
2022-06-19 09:51:23 -0700

    [refactor] Cleaning up

--- hazma/form_factors/vector/_pi_gamma.py
+++ hazma/form_factors/vector/_pi_gamma.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, InitVar
 from typing import Union, overload, Tuple
 
 import numpy as np
@@ -12,23 +12,95 @@ from ._alpha import alpha_em
 MPI0 = parameters.neutral_pion_mass
 
 
+@dataclass(frozen=True)
+class VectorFormFactorPiGammaFitData:
+    """Data for the pion-photon vector form-factor."""
+
+    fpi: float
+
+    amplitude0: float
+    amplitude_rho: float
+    amplitude_omega: float
+    amplitude_phi: float
+
+    mass_rho: float
+    mass_omega: float
+    mass_phi: float
+
+    width_rho: float
+    width_omega: float
+    width_phi: float
+
+
 @dataclass
 class VectorFormFactorPiGamma(VectorFormFactorPA):
+    """Class for computing the pion-photon vector form-factor.
+
+    Attributes
+    ----------
+    fsp_masses: Tuple[float]
+        Final state particle masses (only pion in this case.)
+    fit_data: VectorFormFactorPiGammaFitData
+        Fit information used to compute the form-factor.
+
+    Methods
+    -------
+    form_factor
+        Compute the un-integrated form-factor into a pion and photon.
+    integrated_form_factor
+        Compute the form-factor into a pion and photon integrated over
+        phase-space.
+    width
+        Compute the decay width of a vector into a pion and photon.
+    cross_section
+        Compute the dark matter annihilation cross section into a pion and
+        photon.
+    """
+
     fsp_masses: Tuple[float] = field(init=False, default=(MPI0,))
+    fit_data: VectorFormFactorPiGammaFitData = field(init=False)
 
-    fpi: float = 0.09266
-    amplitude0: float = 0.007594981126020603
-    amplitude_rho: float = 1.0
-    amplitude_omega: float = 0.8846540224221084
-    amplitude_phi: float = -0.06460651106718258
+    fpi: InitVar[float] = 0.09266
+    amplitude0: InitVar[float] = 0.007594981126020603
+    amplitude_rho: InitVar[float] = 1.0
+    amplitude_omega: InitVar[float] = 0.8846540224221084
+    amplitude_phi: InitVar[float] = -0.06460651106718258
 
-    mass_rho: float = 0.77526
-    mass_omega: float = 0.78265
-    mass_phi: float = 1.01946
+    mass_rho: InitVar[float] = 0.77526
+    mass_omega: InitVar[float] = 0.78265
+    mass_phi: InitVar[float] = 1.01946
 
-    width_rho: float = 0.1491
-    width_omega: float = 0.00849
-    width_phi: float = 0.004247
+    width_rho: InitVar[float] = 0.1491
+    width_omega: InitVar[float] = 0.00849
+    width_phi: InitVar[float] = 0.004247
+
+    def __post_init__(
+        self,
+        fpi,
+        amplitude0,
+        amplitude_rho,
+        amplitude_omega,
+        amplitude_phi,
+        mass_rho,
+        mass_omega,
+        mass_phi,
+        width_rho,
+        width_omega,
+        width_phi,
+    ):
+        self.fit_data = VectorFormFactorPiGammaFitData(
+            fpi=fpi,
+            amplitude0=amplitude0,
+            amplitude_rho=amplitude_rho,
+            amplitude_omega=amplitude_omega,
+            amplitude_phi=amplitude_phi,
+            mass_rho=mass_rho,
+            mass_omega=mass_omega,
+            mass_phi=mass_phi,
+            width_rho=width_rho,
+            width_omega=width_omega,
+            width_phi=width_phi,
+        )
 
     def __form_factor(
         self, *, q: RealArray, gvuu: float, gvdd: float, gvss: float
@@ -64,17 +136,17 @@ class VectorFormFactorPiGamma(VectorFormFactorPA):
         cs = -3.0 * gvss
         cd = 2 * gvuu + gvdd
 
-        fpi = self.fpi
+        fpi = self.fit_data.fpi
 
-        a0 = self.amplitude0
-        ar = self.amplitude_rho
-        aw = self.amplitude_omega
-        af = self.amplitude_phi
+        a0 = self.fit_data.amplitude0
+        ar = self.fit_data.amplitude_rho
+        aw = self.fit_data.amplitude_omega
+        af = self.fit_data.amplitude_phi
 
         amp_0 = a0 * 4.0 * np.sqrt(2) * s / (3.0 * fpi)
-        amp_r = ar * amp(ci1, self.mass_rho, self.width_rho)
-        amp_w = aw * amp(ci0, self.mass_omega, self.width_omega)
-        amp_f = af * amp(cs, self.mass_phi, self.width_phi)
+        amp_r = ar * amp(ci1, self.fit_data.mass_rho, self.fit_data.width_rho)
+        amp_w = aw * amp(ci0, self.fit_data.mass_omega, self.fit_data.width_omega)
+        amp_f = af * amp(cs, self.fit_data.mass_phi, self.fit_data.width_phi)
 
         form = amp_0 * (amp_r + amp_w + amp_f) - cd / (4.0 * np.pi**2 * fpi)
         return np.sqrt(4 * np.pi * alpha_em(s)) * form
@@ -90,18 +162,23 @@ class VectorFormFactorPiGamma(VectorFormFactorPA):
     def form_factor(
         self, *, q: Union[float, RealArray], gvuu, gvdd, gvss
     ) -> Union[complex, ComplexArray]:
-        """
-        Compute the pi-gamma-V form factor.
+        """Compute the pion-photon vector form factor.
 
         Parameters
         ----------
-        s: Union[float,npt.NDArray[np.float64]
-            Square of the center-of-mass energy in MeV.
+        q: float or array-like
+            Center of mass energy.
+        gvuu: float
+            Coupling of vector to up-quarks.
+        gvdd: float
+            Coupling of vector to down-quarks.
+        gvss: float
+            Coupling of vector to strange-quarks.
 
         Returns
         -------
-        ff: Union[complex,npt.NDArray[np.complex128]]
-            Form factor from pi-gamma-V.
+        ff: complex or array-like
+            Form-factor.
         """
         single = np.isscalar(q)
         qq = np.atleast_1d(q).astype(np.float64) * 1e-3
@@ -123,4 +200,73 @@ class VectorFormFactorPiGamma(VectorFormFactorPA):
     def width(
         self, mv: Union[float, RealArray], gvuu, gvdd, gvss
     ) -> Union[complex, ComplexArray]:
+        r"""Compute the partial decay width of a massive vector into a neutral
+        pion and photon.
+
+        Parameters
+        ----------
+        mv: float
+            Mass of the vector.
+        gvuu: float
+            Coupling of vector to up-quarks.
+        gvdd: float
+            Coupling of vector to down-quarks.
+        gvss: float
+            Coupling of vector to strange-quarks.
+
+        Returns
+        -------
+        width: float
+            Decay width of vector into a pion and photon.
+        """
         return self._width(mv=mv, gvuu=gvuu, gvdd=gvdd, gvss=gvss)
+
+    def cross_section(
+        self,
+        *,
+        q,
+        mx: float,
+        mv: float,
+        gvxx: float,
+        wv: float,
+        gvuu: float,
+        gvdd: float,
+        gvss: float,
+    ):
+        r"""Compute the cross section for dark matter annihilating into a pion
+        and a photon.
+
+        Parameters
+        ----------
+        q: float
+            Center-of-mass energy.
+        mx: float
+            Mass of the dark matter in MeV.
+        mv: float
+            Mass of the vector mediator in MeV.
+        gvxx: float
+            Coupling of vector to dark matter.
+        wv: float
+            Width of the vector in MeV.
+        gvuu: float
+            Coupling of vector to up-quarks.
+        gvdd: float
+            Coupling of vector to down-quarks.
+        gvss: float
+            Coupling of vector to strange-quarks.
+
+        Returns
+        -------
+        cs: float or array-like
+            Annihilation cross section into two kaons.
+        """
+        return self._cross_section(
+            q=q,
+            mx=mx,
+            mv=mv,
+            gvxx=gvxx,
+            wv=wv,
+            gvuu=gvuu,
+            gvdd=gvdd,
+            gvss=gvss,
+        )
