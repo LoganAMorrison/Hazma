ba2848a1077897d991d3621d6aa89a6f5e6db349
Logan A. Morrison <loganmorrison99@gmail.com>
2022-06-19 08:43:07 -0700

    [refactor] Moving data to separate class

--- hazma/form_factors/vector/_pi_pi.py
+++ hazma/form_factors/vector/_pi_pi.py
@@ -1,7 +1,7 @@
 """
 Module for computing the vector form factor for pi+pi.
 """
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, InitVar
 from typing import Union, Tuple
 
 import numpy as np
@@ -23,23 +23,20 @@ from ._utils import (
 
 
 @dataclass
-class _VectorFormFactorPiPiBase(VectorFormFactorPP):
+class VectorFormFactorPiPiFitData:
+    r"""Class for storing parameters used to compute vector form-factor into two pions.
+
+    Parameters
+    ----------
+    n_max: int
+        Maximum number of resonances.
     """
-    Class for storing the parameters needed to compute the form factor for
-    V-pi-pi.
-    """
-
-    _imode: int
-    _fsp_masses: Tuple[float, float] = field(init=False)
-    fsp_masses: Tuple[float, float] = field(init=False)
-
-    omega_mag: float = field(default=0.00187)
-    omega_phase: float = field(default=0.106)
-    omega_mass: float = field(default=0.7824)
-    omega_width: float = field(default=0.00833)
-    omega_weight: complex = field(default=0j)
-
-    n_max: int = field(default=2000, kw_only=True)
+    n_max: int = field(default=2000)
+    omega_mag: float = field(init=False, default=0.00187, repr=False)
+    omega_phase: float = field(init=False, default=0.106, repr=False)
+    omega_mass: float = field(init=False, default=0.7824, repr=False)
+    omega_width: float = field(init=False, default=0.00833, repr=False)
+    omega_weight: complex = field(init=False, default=0j, repr=False)
 
     masses: RealArray = field(init=False, repr=False)
     widths: RealArray = field(init=False, repr=False)
@@ -55,13 +52,6 @@ class _VectorFormFactorPiPiBase(VectorFormFactorPP):
         self.hres = np.zeros(self.n_max, dtype=np.float64)
         self.h0 = np.zeros(self.n_max, dtype=np.float64)
         self.dh = np.zeros(self.n_max, dtype=np.float64)
-
-        if self._imode == 0:
-            self._fsp_masses = (MPI0_GEV, MPI0_GEV)
-        else:
-            self._fsp_masses = (MPI_GEV, MPI_GEV)
-
-        self.fsp_masses = tuple(m * 1e3 for m in self._fsp_masses)
 
         # Set the rho-parameters
         rho_mag = np.array([1.0, 1.0, 0.59, 4.8e-2, 0.40, 0.43], dtype=np.float64)
@@ -109,7 +99,15 @@ class _VectorFormFactorPiPiBase(VectorFormFactorPP):
         )
         self.dh = np.array(dhhatds(self.masses, self.widths, MPI_GEV, MPI_GEV))
         self.h0 = np.array(
-            h(0.0, self.masses, self.widths, MPI_GEV, MPI_GEV, self.dh, self.hres)
+            h(
+                0.0,
+                self.masses,
+                self.widths,
+                MPI_GEV,
+                MPI_GEV,
+                self.dh,
+                self.hres,
+            )
         )
 
         # fix up the early weights
@@ -118,33 +116,56 @@ class _VectorFormFactorPiPiBase(VectorFormFactorPP):
         rho_sum = np.sum(rho_wgt[1:])
         self.coup[1:nrhowgt] = rho_wgt[1:] * cwgt / rho_sum
 
+
+@dataclass
+class _VectorFormFactorPiPiBase(VectorFormFactorPP):
+    r"""Base class for computing the vector form-factor into two pions."""
+
+    _imode: int
+    fsp_masses: Tuple[float, float] = field(init=False)
+    __fsp_masses: Tuple[float, float] = field(init=False, repr=False)
+    fit_data: VectorFormFactorPiPiFitData = field(init=False)
+    n_max: InitVar[int] = 2000
+
+    def __post_init__(self, n_max):
+        self.fit_data = VectorFormFactorPiPiFitData(n_max)
+
+        if self._imode == 0:
+            self.__fsp_masses = (MPI0_GEV, MPI0_GEV)
+        else:
+            self.__fsp_masses = (MPI_GEV, MPI_GEV)
+
+        self.fsp_masses = tuple(m * 1e3 for m in self.__fsp_masses)
+
     def __form_factor(self, *, s: RealArray, gvuu: float, gvdd: float) -> ComplexArray:
         # Convert gvuu and gvdd to iso-spin couplings
         ci1 = gvuu - gvdd
 
         bw = breit_wigner_gs(
             s,
-            self.masses,
-            self.widths,
+            self.fit_data.masses,
+            self.fit_data.widths,
             MPI_GEV,
             MPI_GEV,
-            self.h0,
-            self.dh,
-            self.hres,
+            self.fit_data.h0,
+            self.fit_data.dh,
+            self.fit_data.hres,
             reshape=True,
         )
 
-        ff = ci1 * self.coup * bw
+        ff = ci1 * self.fit_data.coup * bw
 
         # include rho-omega if needed
         if self._imode != 0:
             ff[:, 0] *= (
                 1.0
-                / (1.0 + self.omega_weight)
+                / (1.0 + self.fit_data.omega_weight)
                 * (
                     1.0
-                    + self.omega_weight
-                    * breit_wigner_fw(s, self.omega_mass, self.omega_width)
+                    + self.fit_data.omega_weight
+                    * breit_wigner_fw(
+                        s, self.fit_data.omega_mass, self.fit_data.omega_width
+                    )
                 )
             )
         # sum
@@ -157,25 +178,26 @@ class _VectorFormFactorPiPiBase(VectorFormFactorPP):
     def form_factor(
         self, *, q: Union[float, RealArray], gvuu, gvdd
     ) -> Union[complex, ComplexArray]:
-        """
-        Compute the pi-pi-V form factor.
+        """Compute the pion-pion form factor.
 
         Parameters
         ----------
-        s: Union[float,RealArray
-            Squared center-of-mass energy in MeV.
-        imode: int, optional
-            Iso-spin channel. Default is 1.
+        q: float or array-like
+            Center of mass energy.
+        gvuu: float
+            Coupling of vector to up-quarks.
+        gvdd: float
+            Coupling of vector to down-quarks.
 
         Returns
         -------
-        ff: Union[complex,ComplexArray]
-            Form factor from pi-pi-V.
+        ff: complex or array-like
+            Form-factor.
         """
 
         single = np.isscalar(q)
         qq = np.atleast_1d(q).astype(np.float64) * 1e-3
-        mask = qq > sum(self._fsp_masses)
+        mask = qq > sum(self.__fsp_masses)
         ff = np.zeros_like(qq, dtype=np.complex128)
 
         ff[mask] = self.__form_factor(
@@ -189,26 +211,120 @@ class _VectorFormFactorPiPiBase(VectorFormFactorPP):
         return ff
 
     def width(
-        self, mv: Union[float, RealArray], gvuu, gvdd
+        self, mv: Union[float, RealArray], gvuu: float, gvdd: float
     ) -> Union[complex, ComplexArray]:
+        r"""Compute the partial decay width of a massive vector into two pions.
+
+        Parameters
+        ----------
+        mv: float
+            Mass of the vector.
+        gvuu: float
+            Coupling of vector to up-quarks.
+        gvdd: float
+            Coupling of vector to down-quarks.
+
+        Returns
+        -------
+        width: float
+            Decay width of vector into two pions.
+        """
         return self._width(mv=mv, gvuu=gvuu, gvdd=gvdd)
+
+    def cross_section(
+        self,
+        *,
+        q,
+        mx: float,
+        mv: float,
+        gvxx: float,
+        wv: float,
+        gvuu: float,
+        gvdd: float,
+    ):
+        r"""Compute the cross section for dark matter annihilating into two
+        pions.
+
+        Parameters
+        ----------
+        q: float
+            Center-of-mass energy.
+        mx: float
+            Mass of the dark matter in MeV.
+        mv: float
+            Mass of the vector mediator in MeV.
+        gvxx: float
+            Coupling of vector to dark matter.
+        wv: float
+            Width of the vector in MeV.
+        gvuu: float
+            Coupling of vector to up-quarks.
+        gvdd: float
+            Coupling of vector to down-quarks.
+
+        Returns
+        -------
+        cs: float or array-like
+            Annihilation cross section into two pions.
+        """
+        return self._cross_section(
+            q=q,
+            mx=mx,
+            mv=mv,
+            gvxx=gvxx,
+            wv=wv,
+            gvuu=gvuu,
+            gvdd=gvdd,
+        )
 
 
 @dataclass
 class VectorFormFactorPiPi(_VectorFormFactorPiPiBase):
-    """
-    Class for storing the parameters needed to compute the form factor for
+    r"""Class for storing the parameters needed to compute the form factor for
     V-pi-pi.
+
+    Attributes
+    ----------
+    n_max: int
+        Maximum number of resonances. Default is 2000.
+
+    Methods
+    -------
+    form_factor
+        Compute the un-integrated form-factor into two-charged pions.
+    integrated_form_factor
+        Compute the form-factor into two-charged pions integrated over
+        phase-space.
+    width
+        Compute the decay width of a vector into two-charged pions.
+    cross_section
+        Compute the dark matter annihilation cross section into two-charged pions.
     """
 
-    _imode: int = 1
+    _imode: int = field(init=False, default=1, repr=False)
 
 
 @dataclass
 class VectorFormFactorPi0Pi0(_VectorFormFactorPiPiBase):
-    """
-    Class for storing the parameters needed to compute the form factor for
+    r"""Class for storing the parameters needed to compute the form factor for
     V-pi0-pi0.
+
+    Attributes
+    ----------
+    n_max: int
+        Maximum number of resonances. Default is 2000.
+
+    Methods
+    -------
+    form_factor
+        Compute the un-integrated form-factor into two neutral pions.
+    integrated_form_factor
+        Compute the form-factor into two neutral pions integrated over
+        phase-space.
+    width
+        Compute the decay width of a vector into two neutral pions.
+    cross_section
+        Compute the dark matter annihilation cross section into two neutral pions.
     """
 
-    _imode: int = 0
+    _imode: int = field(init=False, default=0, repr=False)
