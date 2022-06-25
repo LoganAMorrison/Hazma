0ce6f0afab5c111d54815c8e8f6f22f94384217a
Logan A. Morrison <loganmorrison99@gmail.com>
2022-06-24 17:20:07 -0700

    [bug] Fixed NaN when epi~mpi

--- hazma/spectra/_positron/_pion.pyx
+++ hazma/spectra/_positron/_pion.pyx
@@ -1,4 +1,5 @@
-from libc.math cimport sqrt, pow, log10, fmin, fmax
+from libc.math cimport sqrt, pow, log10, fmin, fmax, fabs
+from libc.float cimport DBL_EPSILON
 import cython
 
 from scipy.integrate import quad
@@ -23,36 +24,6 @@ cdef double beta_mu = sqrt(1.0 - pow(mmu / eng_mu_pi_rf, 2))
 cdef double emax_mu_rf = (me * me + mmu * mmu) / (2.0 * mmu)
 cdef double emax_pi_rf = gamma_mu * emax_mu_rf * (1.0 + beta_mu * sqrt(1.0 - (me / emax_mu_rf)**2))
 
-# cdef double eng_p_max_mu_rf = (me * me + mmu * mmu) / (2.0 * mmu)
-# cdef np.ndarray eng_ps_mu = np.logspace(log10(me), log10(eng_p_max_pi_rf), num=500, dtype=np.float64)
-# cdef np.ndarray __muspec = muspec(eng_ps_mu, eng_mu_pi_rf)
-
-
-# @cython.boundscheck(True)
-# @cython.wraparound(False)
-# @cython.cdivision(True)
-# cdef double __muon_spectrum(double eng_p):
-#     """Returns the muon spectrum in the pion rest frame."""
-#     return np.interp(eng_p, eng_ps_mu, __muspec)
-
-
-# @cython.boundscheck(True)
-# @cython.wraparound(False)
-# @cython.cdivision(True)
-# cdef double __integrand(double cl, double eng_p, double eng_pi):
-#     """Returns the integrand of the boost integral at a given angle."""
-#     if eng_p < me:
-#         return 0.0
-#     cdef double p = sqrt(eng_p * eng_p - me * me)
-#     cdef double gamma = eng_pi / mpi
-#     cdef double beta = sqrt(1.0 - pow(mpi / eng_pi, 2))
-#     cdef double jac = p / (2. * sqrt((1 + pow(beta * cl, 2)) * eng_p * eng_p -
-#                                      (1 + beta * beta * (-1 + cl * cl)) *
-#                                      me * me -
-#                                      2 * beta * cl * eng_p * p) * gamma)
-#     cdef double eng_p_pi_rf = gamma * (eng_p - p * beta * cl)
-#     return BR_PI_TO_MU_NUMU * jac * __muon_spectrum(eng_p_pi_rf)
-
 
 @cython.boundscheck(True)
 @cython.wraparound(False)
@@ -69,12 +40,13 @@ cdef double dnde_positron_charged_pion_point(double e, double epi):
     cdef double beta
     cdef double emin
     cdef double emax
-    cdef double e0
-    cdef double delta
     cdef double dnde_mu
     cdef double dnde_e
 
     if epi < mpi or e < me:
+        return 0.0
+
+    if fabs(epi - mpi) < DBL_EPSILON:
         return 0.0
 
     gamma = boost_gamma(epi, mpi)
