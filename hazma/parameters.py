e1a3264f4ae3e4f5742f95e13c088d1489d1caf0
Logan A. Morrison <loganmorrison99@gmail.com>
2022-06-24 17:21:21 -0700

    [feat] Adding more parameters
    - Added exact conversion factors
    - Added remaining SM masses
    - Added dictionaries for groups (masses, widths, etc.)
    - Added PDG codes
    - Added doc strings
    - Added CKM and added Wolfenstein parameters

--- hazma/parameters.py
+++ hazma/parameters.py
@@ -1,3 +1,5 @@
+from typing import Dict, List
+
 import numpy as np
 from scipy.integrate import trapz
 from scipy.interpolate import InterpolatedUnivariateSpline, interp1d
@@ -6,64 +8,103 @@ from scipy.interpolate import InterpolatedUnivariateSpline, interp1d
 Physics constants and utility functions.
 """
 
-# Masses (MeV)
+# ==============================================================================
+# ---- Conversion Factors ------------------------------------------------------
+# ==============================================================================
+
+#: :math:`c` - Speed of light in m/s
+speed_of_light = 299792458
+
+_hbar_joule_sec = 6.62607015e-34 / (2 * np.pi)  # J.s
+_joule_to_mev = 1 / (1.602176634e-19) * 1e-6  # MeV
+
+# hbar [eV * s] = 1 => s = 1 / (hbar [eV]) = 1 / (hbar [1e-6 * MeV])
+
+#: Convert :math:`\mathrm{s}` to :math:`\mathrm{MeV}^{-1}`
+s_to_inv_MeV = 1.0 / (_hbar_joule_sec * _joule_to_mev)  # MeV^-1
+#: Convert :math:`\mathrm{m}` to :math:`\mathrm{MeV}^{-1}`
+m_to_inv_MeV = s_to_inv_MeV / speed_of_light  # MeV^-1
+#: Convert :math:`\mathrm{cm}` to :math:`\mathrm{MeV}^{-1}`
+cm_to_inv_MeV = m_to_inv_MeV * 1e-2  # MeV^-1 cm^-1
+#: Convert :math:`\expval{\sigma v}` from
+#: :math:`\mathrm{MeV}^{-2}` to :math:`\mathrm{cm}^{3}/\mathrm{s}`
+sv_inv_MeV_to_cm3_per_s = 1.0 / cm_to_inv_MeV**2 * speed_of_light * 1e2
+#: Convert :math:`\mathrm{grams}` to :math:`\mathrm{MeV}`
+g_to_MeV = s_to_inv_MeV / m_to_inv_MeV**2 / _hbar_joule_sec * 1e-3
+#: Convert :math:`\mathrm{MeV}` to :math:`\mathrm{grams}`
+MeV_to_g = 1 / g_to_MeV
+#: Solar mass to grams
+Msun_to_g = 1.988e33
+#: Gram to solar mass
+g_to_Msun = 1 / Msun_to_g
+#: Barn to MeV^-2
+barn_to_inv_MeV2 = 0.00256819
+
+# ==============================================================================
+# ---- Masses (MeV) ------------------------------------------------------------
+# ==============================================================================
 
 #: Higgs mass in MeV
-higgs_mass = 125.1e3
+higgs_mass: float = 125.1e3
 #: Electron mass in MeV
-electron_mass = 0.5109989461  # m[e-] = 0.5109989461 ± 3.1e-09
+electron_mass: float = 0.5109989461  # m[e-] = 0.5109989461 ± 3.1e-09
 #: Muon mass in MeV
-muon_mass = 105.6583745  # m[mu-] = 105.6583745 ± 2.4e-06
+muon_mass: float = 105.6583745  # m[mu-] = 105.6583745 ± 2.4e-06
 #: Tau mass in MeV
-tau_mass = 1776.86  # m[tau-] = 1776.86 ± 0.12
+tau_mass: float = 1776.86  # m[tau-] = 1776.86 ± 0.12
 #: Neutral pion mass in MeV
-neutral_pion_mass = 134.9768  # m[pi0] = 134.9768 ± 0.0005
+neutral_pion_mass: float = 134.9768  # m[pi0] = 134.9768 ± 0.0005
 #: Charged pion mass in MeV
-charged_pion_mass = 139.57039  # m[pi+] = 139.57039 ± 0.00018
+charged_pion_mass: float = 139.57039  # m[pi+] = 139.57039 ± 0.00018
 #: Eta mass in MeV
-eta_mass = 547.862  # m[eta] = 547.862 ± 0.017
+eta_mass: float = 547.862  # m[eta] = 547.862 ± 0.017
 #: Eta' mass in MeV
-eta_prime_mass = 957.78  # m[eta'(958)] = 957.78 ± 0.06
+eta_prime_mass: float = 957.78  # m[eta'(958)] = 957.78 ± 0.06
 #: Charged kaon mass in MeV
-charged_kaon_mass = 493.677  # m[K+] = 493.677 ± 0.016
+charged_kaon_mass: float = 493.677  # m[K+] = 493.677 ± 0.016
 #: Neutral kaon mass in MeV
-neutral_kaon_mass = 497.611  # m[K0] = 497.611 ± 0.013
+neutral_kaon_mass: float = 497.611  # m[K0] = 497.611 ± 0.013
 #: Long kaon mass in MeV
-long_kaon_mass = 497.611  # m[K(L)0] = 497.611 ± 0.013
+long_kaon_mass: float = 497.611  # m[K(L)0] = 497.611 ± 0.013
 #: Short kaon mass in MeV
-short_kaon_mass = 497.611  # m[K(S)0] = 497.611 ± 0.013
+short_kaon_mass: float = 497.611  # m[K(S)0] = 497.611 ± 0.013
 #: Rho mass in MeV
-rho_mass = 775.26  # m[rho(770)0] = 775.26 ± 0.23
+rho_mass: float = 775.26  # m[rho(770)0] = 775.26 ± 0.23
 #: Omega mass in MeV
-omega_mass = 782.66  # m[omega(782)] = 782.66 ± 0.13
+omega_mass: float = 782.66  # m[omega(782)] = 782.66 ± 0.13
 #: Phi mass in MeV
-phi_mass = 1019.461  # m[phi(1020)] = 1019.461 ± 0.016
+phi_mass: float = 1019.461  # m[phi(1020)] = 1019.461 ± 0.016
 #: Charged B mass in MeV
-charged_B_mass = 5279.29  # B^+ meson
+charged_B_mass: float = 5279.29  # B^+ meson
 #: Pion mass in MeV (chiral-limit)
-pion_mass_chiral_limit = (neutral_pion_mass + charged_pion_mass) / 2.0
+pion_mass_chiral_limit: float = (neutral_pion_mass + charged_pion_mass) / 2.0
 #: Kaon mass in MeV (chiral-limit)
-kaon_mass_chiral_limit = (neutral_kaon_mass + charged_kaon_mass) / 2.0
+kaon_mass_chiral_limit: float = (neutral_kaon_mass + charged_kaon_mass) / 2.0
 
-
-# Quark masses in MS-bar scheme
 #: Up-quark mass in MeV
-up_quark_mass = 2.3
+up_quark_mass: float = 2.16
 #: Down-quark mass in MeV
-down_quark_mass = 4.8
+down_quark_mass: float = 4.67
 #: Strange-quark mass in MeV
-strange_quark_mass = 95.0
+strange_quark_mass: float = 95.0
 #: Charm-quark mass in MeV
-charm_quark_mass = 1.275e3
+charm_quark_mass: float = 1.27e3
 #: Bottom-quark mass in MeV
-bottom_quark_mass = 4.18e3
+bottom_quark_mass: float = 4.18e3
 #: Top-quark mass in MeV
-top_quark_mass = 160.0e3
+top_quark_mass: float = 172.9e3
+
+# Vector Boson Masses
+
+#: W-Boson mass in MeV
+wboson_mass: float = 80.385003e-3
+#: Z-Boson mass in MeV
+zboson_mass: float = 91.18760e-3
 
 # Collections of masses
-lepton_masses = [electron_mass, muon_mass]
+lepton_masses: List[float] = [electron_mass, muon_mass]
 
-standard_model_masses = {
+standard_model_masses: Dict[str, float] = {
     "e": electron_mass,
     "mu": muon_mass,
     "tau": tau_mass,
@@ -86,72 +127,215 @@ standard_model_masses = {
     "eta": eta_mass,
     "etap": eta_prime_mass,
     "rho": rho_mass,
+    "rho0": rho_mass,
     "omega": omega_mass,
     "phi": phi_mass,
 }
 
+# ==============================================================================
+# ---- Widths ------------------------------------------------------------------
+# ==============================================================================
 
-#: Convert :math:`\mathrm{cm}` to :math:`\mathrm{MeV}^{-1}`
-cm_to_inv_MeV = 5.08e10  # MeV^-1 cm^-1
-#: Convert :math:`\expval{\sigma v}` from
-#: :math:`\mathrm{MeV}^{-2}` to :math:`\mathrm{cm}^{3}/\mathrm{s}`
-sv_inv_MeV_to_cm3_per_s = 1.0 / cm_to_inv_MeV**2 * 3e10  # cm^3/s * MeV^2
-#: Convert :math:`\mathrm{grams}` to :math:`\mathrm{MeV}`
-g_to_MeV = 5.61e26
-#: Convert :math:`\mathrm{MeV}` to :math:`\mathrm{grams}`
-MeV_to_g = 1 / g_to_MeV
-#: Solar mass to grams
-Msun_to_g = 1.988e33
-#: Gram to solar mass
-g_to_Msun = 1 / Msun_to_g
+# Widths (MeV)
+rho_width: float = 147.4
+B_width = 4.35e-10
+k_width = 5.32e-14
+kl_width = 1.29e-14
+
+SM_WIDTHS: Dict[str, float] = {
+    "mu": 1 / (2.1969811e-6 * s_to_inv_MeV),
+    "tau": 1 / (290.3e-15 * s_to_inv_MeV),
+    "t": 1.42e3,
+    "z": 2.49520e3,
+    "w": 2.08500e3,
+    "h": 0.00374e3,
+    "pi0": 1 / (8.43e-17 * s_to_inv_MeV),
+    "pi": 1 / (2.6033e-8 * s_to_inv_MeV),
+    "k": 1 / (1.2380e-8 * s_to_inv_MeV),
+    "ks": 1 / (0.8954e-10 * s_to_inv_MeV),
+    "kl": 1 / (5.116e-8 * s_to_inv_MeV),
+    "eta": 1.31e-3,
+    "rho": 147.4,
+    "omega": 8.68,
+    "eta_prime": 0.188,
+    "phi": 4.249,
+}
+
+# ==============================================================================
+# ---- PDG Codes ---------------------------------------------------------------
+# ==============================================================================
+
+PDG_CODES: Dict[str, int] = {
+    "d": 1,
+    "u": 2,
+    "s": 3,
+    "c": 4,
+    "b": 5,
+    "t": 6,
+    "e": 11,
+    "mu": 13,
+    "tau": 15,
+    "ve": 12,
+    "vm": 14,
+    "vt": 16,
+    "g": 21,
+    "a": 22,
+    "z": 23,
+    "w": 24,
+    "h": 25,
+    "pi0": 111,
+    "pi": 211,
+    "k0": 311,
+    "kl": 130,
+    "ks": 310,
+    "k": 321,
+    "eta": 221,
+    "etap": 331,
+    "rho0": 113,
+    "rho": 213,
+    "omega": 223,
+    "phi": 333,
+}
+
 
 # Miscellaneous
-alpha_em = 1.0 / 137.04  # fine structure constant.
-GF = 1.1663787e-11  # Fermi constant in MeV**-2
-vh = 246.22795e3  # Higgs VEV in MeV
-qe = np.sqrt(4.0 * np.pi * alpha_em)
-temp_cmb_formation = 0.235e-6  # CMB temperature at formation in MeV
-plank_mass = 1.22091e22  # MeV
-rho_crit = 1.05375e-2  # Critical energy density in units of h^2 MeV / cm^3
-sm_entropy_density_today = 2891.2  # Entropy density today in units of cm^-3
-omega_h2_cdm = 0.1198  # Dark matter energy fraction times h^2
-dimensionless_hubble_constant = 0.6774  # H0 = h * (100 km / s / Mpc), Planck 2015
-sin_theta_weak_sqrd = 0.22290
-sin_theta_weak = np.sqrt(sin_theta_weak_sqrd)
-cos_theta_weak = np.sqrt(1.0 - sin_theta_weak_sqrd)
+#: Electromagnetic fine structure constant.
+alpha_em: float = 1.0 / 137.04
+#: Fermi constant in MeV**-2
+GF: float = 1.1663787e-11
+#: Higg vacuum expectation value in MeV
+vh: float = 246.22795e3
+#:  :math:`e` - Electromagnetic coupling constant
+qe: float = np.sqrt(4.0 * np.pi * alpha_em)
+#: :math:`T_{\mathrm{CMB}}` - CMB temperature at formation in MeV
+temp_cmb_formation: float = 0.235e-6
+#: :math:`M_{\mathrm{pl}}` - Plank mass in MeV
+plank_mass: float = 1.22091e22
+#: :math:`\rho_{\mathrm{crit}}` - Critical energy density in units of
+#: :math:`h^2 \ [\mathrm{MeV} / \mathrm{cm}^3]`
+rho_crit: float = 1.05375e-2
+#: :math:`s_{0}` - Entropy density today [:math:`\mathrm{cm}^{-3}`]
+sm_entropy_density_today: float = 2891.2
+#: :math:`\Omega_{\mathrm{CMD}}h^2` - Energy density fraction of dark matter
+#: times :math:`h^2`
+omega_h2_cdm: float = 0.1198
+#: :math:`h = H_{0}/(100 \mathrm{km/s/Mpc})` - Hubble constant scale by
+#: 100 km/s/Mpc (Plank 2015)
+dimensionless_hubble_constant: float = 0.6774
+#: :math:`\sin^2\theta_{W}` - Square of the sine of the Weinberg angle
+sin_theta_weak_sqrd: float = 0.22290
+#: :math:`\sin\theta_{W}` - Sine of the Weinberg angle
+sin_theta_weak: float = np.sqrt(sin_theta_weak_sqrd)
+#: :math:`\cos\theta_{W}` - Cosine of the Weinberg angle
+cos_theta_weak: float = np.sqrt(1.0 - sin_theta_weak_sqrd)
 
-# Charges
-Qu = 2.0 / 3.0
-Qd = -1.0 / 3.0
-Qe = -1.0
+#: :math:`Q_{u}/e` - Up-type quark charge in units of electron charge.
+Qu: float = 2.0 / 3.0
+#: :math:`Q_{d}/e` - Down-type quark charge in units of electron charge.
+Qd: float = -1.0 / 3.0
+#: :math:`Q_{e}/e` - Charged lepton charge in units of electron charge.
+Qe: float = -1.0
+
+# ==============================================================================
+# ---- CKM matrix elements -----------------------------------------------------
+# ==============================================================================
+
+
+def _ckm_wolfenstein(lam, a, rhob, etab):
+    z = rhob + 1j * etab
+    alam = a * lam**2
+
+    s12 = lam
+    s23 = alam
+    s13d = (
+        a
+        * lam**3
+        * z
+        * np.sqrt(1 - alam**2)
+        / (np.sqrt(1 - lam**2) * (1 - alam**2 * z))
+    )
+
+    c12 = np.sqrt(1 - s12**2)
+    c23 = np.sqrt(1 - s23**2)
+    c13 = np.sqrt(1 - abs(s13d) ** 2)
+
+    m1 = np.array([[1.0, 0.0, 0.0], [0.0, c23, s23], [0.0, -s23, c23]])
+    m2 = np.array([[c13, 0.0, s13d.conj()], [0.0, 1.0, 0.0], [-s13d, 0.0, c13]])
+    m3 = np.array([[c12, s12, 0.0], [-s12, c12, 0.0], [0.0, 0.0, 1.0]])
+
+    return (m1 @ m2) @ m3
+
+
+# PDG March 2022
+_wolfenstein_lam = 0.22500
+_wolfenstein_a = 0.826
+_wolfenstein_rhob = 0.159
+_wolfenstein_etab = 0.348
+
+
+#: :math:`V_{\mathrm{CKM}}` - CKM matrix
+CKM = _ckm_wolfenstein(
+    _wolfenstein_lam, _wolfenstein_a, _wolfenstein_rhob, _wolfenstein_etab
+)
+#: :math:`V_{ud}` - up-down CKM matrix element
+Vud: complex = CKM[0, 0]
+#: :math:`V_{us}` - up-strange CKM matrix element
+Vus: complex = CKM[0, 1]
+#: :math:`V_{ub}` - up-bottom CKM matrix element
+Vub: complex = CKM[0, 2]
+
+#: :math:`V_{ud}` - charm-down CKM matrix element
+Vcd: complex = CKM[1, 0]
+#: :math:`V_{us}` - charm-strange CKM matrix element
+Vcs: complex = CKM[1, 1]
+#: :math:`V_{ub}` - charm-bottom CKM matrix element
+Vcb: complex = CKM[1, 2]
+
+#: :math:`V_{td}` - top-down CKM matrix element
+Vtd: complex = CKM[2, 0]
+#: :math:`V_{ts}` - top-strange CKM matrix element
+Vts: complex = CKM[2, 1]
+#: :math:`V_{tb}` - top-bottom CKM matrix element
+Vtb: complex = CKM[2, 2]
+
+
+# ==============================================================================
+# ---- ChiPT -------------------------------------------------------------------
+# ==============================================================================
 
 # Low Energy constants
-fpi0 = 91.924  # Neutral pion decay constant
-fpi = 92.2138  # Charged pion decay constant
-fk = 110.379  # Charged kaon decay constant
-feta = 57.8
-b0 = pion_mass_chiral_limit**2 / (up_quark_mass + down_quark_mass)
-G8 = 5.47
-G27 = 0.392
-gv = 67.0
-fv = 153.0
+#: :math:`f_{\pi^{0}}` - Neutral pion decay constant.
+fpi0: float = 91.924
+#: :math:`f_{\pi^{\pm}}` - Charged pion decay constant.
+fpi: float = 92.2138
+#: :math:`f_{K^{\pm}}` - Charged kaon decay constant.
+fk: float = 110.379
+#: :math:`f_{\eta}` - Eta decay constant.
+feta: float = 57.8
+#: :math:`B_{0} = \expval{\bar{q}q}/(3f_{\pi}^2)` - ChiPT mass parameter
+b0: float = pion_mass_chiral_limit**2 / (up_quark_mass + down_quark_mass)
+
+G8: float = 5.47
+G27: float = 0.392
+gv: float = 67.0
+fv: float = 153.0
 
 # The following low energy constants are for NLO ChiPT, evaluated at mu = mrho.
-nlo_lec_mu = rho_mass
-Lr1 = 0.56 * 1.0e-3
-Lr2 = 1.21 * 1.0e-3
-L3 = -2.79 * 1.0e-3
-Lr4 = -0.36 * 1.0e-3
-Lr5 = 1.4 * 1.0e-3
-Lr6 = 0.07 * 1.0e-3
-L7 = -0.44 * 1.0e-3
-Lr8 = 0.78 * 1.0e-3
+nlo_lec_mu: float = rho_mass
+Lr1: float = 0.56 * 1.0e-3
+Lr2: float = 1.21 * 1.0e-3
+L3: float = -2.79 * 1.0e-3
+Lr4: float = -0.36 * 1.0e-3
+Lr5: float = 1.4 * 1.0e-3
+Lr6: float = 0.07 * 1.0e-3
+L7: float = -0.44 * 1.0e-3
+Lr8: float = 0.78 * 1.0e-3
 
 # SU(2) LECs
-Er = 0.029
-Gr = 0.0073
+Er: float = 0.029
+Gr: float = 0.0073
 
-LECS = {
+LECS: Dict[str, float] = {
     "1": Lr1,
     "2": Lr2,
     "3": L3,
@@ -165,22 +349,8 @@ LECS = {
 }
 
 # SU(2) LECs
-Er = 0.029
-Gr = 0.0073
-
-# CKM matrix elements
-Vud = 0.974267
-Vus = 0.2248
-Vts = -0.0405 - 0.00075987j
-Vtb = 0.999139
-Vtd = 0.00823123 - 0.00328487j
-
-
-# Widths (MeV)
-rho_width = 146.2
-B_width = 4.35e-10
-k_width = 5.32e-14
-kl_width = 1.29e-14
+Er: float = 0.029
+Gr: float = 0.0073
 
 
 def convert_sigmav(sv, target):
