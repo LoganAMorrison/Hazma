a38ff5022493270d9db7249eab20a242c1fe960d
Logan A. Morrison <loganmorrison99@gmail.com>
2022-07-06 12:37:21 -0700

    [test] Adding tests for phase space and rhn

---
+++ test/rh_neutrino/integration.py
@@ -0,0 +1,97 @@
+import unittest
+
+from pytest import approx
+
+import numpy as np
+from numpy import testing as np_testing
+
+from hazma.parameters import standard_model_masses as sm_masses
+from hazma import rh_neutrino
+from hazma.utils import lnorm_sqr
+
+ME = sm_masses["e"]
+MMU = sm_masses["mu"]
+MPI0 = sm_masses["pi0"]
+MPI = sm_masses["pi"]
+MRHO = sm_masses["rho"]
+MOMEGA = sm_masses["omega"]
+MPHI = sm_masses["phi"]
+
+
+class TestWidths(unittest.TestCase):
+    def setUp(self) -> None:
+        masses = {
+            "e": [
+                0.5109989461,
+                53.59568566915,
+                120.57308672305,
+                137.52909447305,
+                175.69906897304998,
+                243.18746897305,
+                277.09948447305,
+                386.66438947305005,
+                521.02499947305,
+                661.5609999999999,
+                775.51549947305,
+                779.2154994730499,
+                901.0605,
+                1398.4159994730499,
+            ],
+            "mu": [
+                0.5109989461,
+                53.59568566915,
+                120.57308672305,
+                173.1467745,
+                228.27275674999999,
+                262.18477225000004,
+                329.67317225,
+                464.03378225,
+                573.59868725,
+                687.2976872500001,
+                778.96,
+                831.7891872499999,
+                950.18968725,
+                1450.9896872499999,
+            ],
+            "tau": [
+                0.5109989461,
+                67.9993989461,
+                173.1467745,
+                245.2287645,
+                413.50139,
+                661.5609999999999,
+                778.96,
+                901.0605,
+                1398.4159994730499,
+            ],
+        }
+        self.models = {
+            key: [rh_neutrino.RHNeutrino(mass, 1e-3, key) for mass in ms]
+            for key, ms in masses.items()
+        }
+
+        self.thresholds = {}
+        for key, models in self.models.items():
+            self.thresholds[key] = {}
+            for states in models[0].list_decay_final_states():
+                self.thresholds[key][states] = sum(
+                    map(lambda s: sm_masses[s], states.split(" "))
+                )
+
+    def test_decay_widths(self):
+        """Test RHNeutrino.decay_widths"""
+
+        for key, models in self.models.items():
+            for model in models:
+                widths = model._decay_widths()
+                for states, width in widths.items():
+                    if self.thresholds[key][states] > model.mx:
+                        assert width == 0.0
+
+    def test_dnde_photon(self):
+        """Test RHNeutrino.decay_widths"""
+
+        for key, models in self.models.items():
+            for model in models:
+                es = np.geomspace(1e-2, 1, 5) * model.mx
+                model.spectra(es)
---
+++ test/test_phase_space.py
@@ -0,0 +1,114 @@
+import unittest
+
+import numpy as np
+from numpy.testing import assert_allclose
+
+from hazma import parameters
+from hazma.parameters import GF, alpha_em
+from hazma.parameters import electron_mass as me
+from hazma.parameters import muon_mass as mmu
+from hazma.parameters import qe
+from hazma.phase_space import Rambo
+from hazma.utils import ldot
+
+mw = parameters.wboson_mass
+mz = parameters.zboson_mass
+
+
+class TestRambo(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_compute_annihilation_cross_section(self):
+        def msqrd_ee_to_mumu(momenta):
+            p3 = momenta[:, 0]
+            p4 = momenta[:, 1]
+
+            Q = np.sum(momenta, axis=1)[0]
+            pi_mag = np.sqrt(Q**2 / 4.0 - me**2)
+            p1 = np.zeros_like(p3)
+            p2 = np.zeros_like(p3)
+
+            p1[0] = 0.5 * Q
+            p2[0] = 0.5 * Q
+            p1[3] = pi_mag
+            p2[3] = -pi_mag
+
+            return (
+                2.0
+                * qe**4
+                * (
+                    ldot(p1, p4) * ldot(p2, p3)
+                    + ldot(p1, p3) * ldot(p2, p4)
+                    + ldot(p1, p2) * me**2
+                    + ldot(p3, p4) * mmu**2
+                    + 2.0 * me**2 * mmu**2
+                )
+            ) / (ldot(p3, p4) + mmu**2) ** 2
+
+        fsp_masses = np.array([mmu, mmu])
+        cme = 1000.0
+
+        rambo = Rambo(cme, fsp_masses, msqrd=msqrd_ee_to_mumu).cross_section(
+            m1=me, m2=me, n=5000
+        )
+        analytic = 4.0 * np.pi * alpha_em**2 / (3.0 * cme**2)
+        assert_allclose(rambo[0], analytic, rtol=5e-3)
+
+    def test_compute_decay_width_muon(self):
+        """
+        Test rambo decay width function on mu -> e nu nu.
+        """
+
+        def msqrd_mu_to_enunu(momenta):
+            """
+            Matrix element squared for mu -> e nu nu.
+            """
+            pe = momenta[:, 0]
+            pve = momenta[:, 1]
+            pvmu = momenta[:, 2]
+
+            pmu = np.sum(momenta, axis=1)
+
+            return 64.0 * GF**2 * ldot(pe, pvmu) * ldot(pmu, pve)
+
+        fsp_masses = np.array([me, 0.0, 0.0])
+        rambo = Rambo(mmu, fsp_masses, msqrd=msqrd_mu_to_enunu).decay_width(n=50_000)
+
+        r = me**2 / mmu**2
+        corr_fac = 1.0 - 8.0 * r + 8 * r**3 - r**4 - 12.0 * r**2 * np.log(r)
+        analytic = GF**2 * mmu**5 / (192.0 * np.pi**3) * corr_fac
+
+        assert_allclose(rambo[0], analytic, rtol=5e-3)
+
+    def test_compute_decay_width_Zee(self):
+        """
+        Test rambo decay width function on Z -> e e.
+        """
+        sw = parameters.sin_theta_weak
+        cw = np.sqrt(1.0 - sw**2)
+        fsp_masses = np.array([me, me])
+
+        def msqrd_z_to_ee(momenta):
+            p1 = momenta[:, 0]
+            p2 = momenta[:, 1]
+
+            return (
+                qe**2
+                * (
+                    2 * (1 - 4 * sw**2 + 8 * sw**4) * ldot(p1, p2) ** 2
+                    + 2 * (1 - 4 * sw**2 + 8 * sw**4) * me**4
+                    + 12 * sw**2 * (-1 + 2 * sw**2) * me**2 * mz**2
+                    + (1 - 4 * sw**2 + 8 * sw**4)
+                    * ldot(p1, p2)
+                    * (4 * me**2 + mz**2)
+                )
+            ) / (6.0 * cw**2 * sw**2 * mz**2)
+
+        rambo = Rambo(mmu, fsp_masses, msqrd=msqrd_z_to_ee).decay_width(n=10)
+
+        num = qe**2 * (8.0 * sw**4 - 4.0 * sw**2 + 1) * mz
+        den = 96.0 * np.pi * cw**2 * sw**2
+        analytic = num / den
+
+        assert_allclose(rambo[0], analytic, rtol=5e-3)
