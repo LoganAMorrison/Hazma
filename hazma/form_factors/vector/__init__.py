95c9fcab21d680993433a971d7a0b4c8b2716b39
Logan A. Morrison <loganmorrison99@gmail.com>
2022-07-06 12:40:27 -0700

    [doc][feat] Updating docs and exporting couplings

--- hazma/form_factors/vector/__init__.py
+++ hazma/form_factors/vector/__init__.py
@@ -1,3 +1,32 @@
+"""
+The vector form factor library.
+
+This module contains the implementation of 20 different vector form factors.
+
+π⁺π⁻:     VectorFormFactorPiPi
+π⁰π⁰:     VectorFormFactorPi0Pi0
+K⁺K:      VectorFormFactorKK
+K⁰K̅⁰:     VectorFormFactorK0K0
+π⁰Ɣ:      VectorFormFactorPi0Gamma
+π⁰ω:      VectorFormFactorPi0Omega
+π⁰ɸ:      VectorFormFactorPi0Phi
+ηƔ:       VectorFormFactorEtaGamma
+ηω:       VectorFormFactorEtaOmega
+ηɸ:       VectorFormFactorEtaPhi
+π⁰K⁰K̅⁰:   VectorFormFactorPi0K0K0
+π⁰K⁺K⁻:   VectorFormFactorPi0KpKm
+π⁺K⁺K⁰:   VectorFormFactorPiKK0
+ηπ⁺π⁻:    VectorFormFactorPiPiEta
+η'π⁺π⁻:   VectorFormFactorPiPiEtaPrime
+ωπ⁺π⁻:    VectorFormFactorPiPiOmega
+ωπ⁰π⁰:    VectorFormFactorPi0Pi0Omega
+π⁺π⁻π⁰:   VectorFormFactorPiPiPi0
+π⁺π⁻π⁰π⁰: VectorFormFactorPiPiPi0Pi0
+π⁺π⁻π⁺π⁻: VectorFormFactorPiPiPiPi
+
+"""
+
+from ._base import VectorFormFactorCouplings
 from ._pi_pi import VectorFormFactorPiPi, VectorFormFactorPi0Pi0
 from ._k_k import VectorFormFactorKK, VectorFormFactorK0K0
 from ._pi_gamma import VectorFormFactorPi0Gamma
@@ -38,4 +67,5 @@ __all__ = [
     "VectorFormFactorPiPiPi0",
     "VectorFormFactorPiPiPi0Pi0",
     "VectorFormFactorPiPiPiPi",
+    "VectorFormFactorCouplings",
 ]
